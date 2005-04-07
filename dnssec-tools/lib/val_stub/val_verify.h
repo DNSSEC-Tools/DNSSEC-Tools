fb015dfa1cee5d59500d5d35abd8488bff588bde
ahayatnagarkar <ahayatnagarkar@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-04-07 21:37:26 +0000

    Functions to verify the RRSIG signatures, given the original rrset and the DNSKEY.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@329 c4029f9c-b713-0410-a448-fae9e31a314a

---
+++ dnssec-tools/lib/val_stub/val_verify.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2005 Sparta, Inc.  All rights reserved.
+ * See the COPYING file distributed with this software for details.
+ *
+ * Author: Abhijit Hayatnagarkar
+ *
+ * This is the header file for the verifier.
+ */
+
+#ifndef VAL_VERIFY_H
+#define VAL_VERIFY_H
+
+#include <resolver.h>
+#include "validator.h"
+#include "val_errors.h"
+
+/*
+ * Result status codes returned by the validator functions.
+ */
+
+typedef int val_result_t;
+
+/*
+ * The Verifier Function.
+ *
+ * Returns VALIDATE_SUCCESS if DNSSEC validation succeeds,
+ *         INDETERMINATE if DNSSEC validation fails
+ *         and other values as given above.
+ * It takes in a domain_info struct, and does not make any resolver queries itself.
+ * It's just a passive verifier.
+ */
+val_result_t val_verify (struct val_context *context, struct domain_info *response);
+
+#endif
