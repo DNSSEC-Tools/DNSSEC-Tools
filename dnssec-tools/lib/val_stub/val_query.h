19d3924195a3c04a842581c47d38ca145483f222
Abhijit Hayatnagarkar <ahayatnagarkar>
2005-04-15 18:16:47 +0000

    A wrapper function val_query() around res_squery() and val_verify().  This function returns the response along with the dnssec-status.

---
+++ dnssec-tools/lib/val_stub/val_query.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2005 Sparta, Inc.  All rights reserved.
+ * See the COPYING file distributed with this software for details.
+ *
+ * Author: Abhijit Hayatnagarkar
+ *
+ * This is the header file for a wrapper function around the
+ * secure resolver and the verifier.  Applications should be able to
+ * use this with minimal change.
+ */
+
+#ifndef VAL_QUERY_H
+#define VAL_QUERY_H
+
+/*
+ * Returns the length (in bytes) of the answer on success, and -1 on
+ * failure.  If successful, *dnssec_status will contain VALIDATE_SUCCESS
+ * If there is a failure, *dnssec_status will contain the validator
+ * error code.
+ */
+int val_query ( const char *domain_name, int class, int type,
+		unsigned char *answer, int anslen,
+		int *dnssec_status );
+#endif
