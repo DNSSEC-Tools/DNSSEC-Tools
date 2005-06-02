700cbacbf10b43e06d13015630e7b171cc97674d
Suresh Krishnaswamy <hserus>
2005-06-02 21:22:19 +0000

    Added few more prototypes

--- dnssec-tools/lib/val_stub/val_x_query.h
+++ dnssec-tools/lib/val_stub/val_x_query.h
@@ -3,19 +3,15 @@
  * Copyright 2005 SPARTA, Inc.  All rights reserved.
  * See the COPYING file distributed with this software for details.
  */ 
-#ifndef VAL_QUERY_EX_H
-#define VAL_QUERY_EX_H
+#ifndef VAL_X_QUERY_H
+#define VAL_X_QUERY_H
 
-void lower_name (u_int8_t rdata[], int *index);
-int assimilate_answers(val_context_t *context, struct query_chain **queries, 
-							struct domain_info *response, struct query_chain *matched_q, 
-								struct assertion_chain **assertions);
 int val_x_query(val_context_t *ctx,
             const char *domain_name,
-            const u_int16_t type,
             const u_int16_t class,
-            const u_int16_t flags,
+            const u_int16_t type,
+			const u_int8_t flags,
             struct response_t *resp,
-            int resp_count);
+            int *resp_count);
 
-#endif /* VAL_QUERY_EX_H */
+#endif /* VAL_X_QUERY_H */
