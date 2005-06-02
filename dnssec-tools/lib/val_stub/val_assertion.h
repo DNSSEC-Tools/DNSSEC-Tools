477ac614e54636d55cd24d80282984c4f7ae5b2b
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-06-02 21:05:16 +0000

    New file containing logic for the assertion chains
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@577 c4029f9c-b713-0410-a448-fae9e31a314a

---
+++ dnssec-tools/lib/val_stub/val_assertion.h
@@ -0,0 +1,36 @@
+
+/*
+ * Copyright 2005 SPARTA, Inc.  All rights reserved.
+ * See the COPYING file distributed with this software for details.
+ */ 
+#ifndef VAL_ASSERTION_H
+#define VAL_ASSERTION_H
+
+struct val_result {
+	struct assertion_chain *as;
+	int status;
+	int trusted;
+	struct val_result *next;
+};
+
+void lower_name (u_int8_t rdata[], int *index);
+int assimilate_answers(val_context_t *context, struct query_chain **queries, 
+							struct domain_info *response, struct query_chain *matched_q, 
+								struct assertion_chain **assertions);
+int add_to_query_chain(struct query_chain **queries, u_char *name_n, 
+						const u_int16_t type_h, const u_int16_t class_h);
+int add_to_assertion_chain(struct assertion_chain **assertions, struct rrset_rec *response_data);
+void free_assertion_chain(struct assertion_chain **assertions);
+void free_result_chain(struct val_result **results);
+void free_query_chain(struct query_chain **queries);
+
+int resolve_n_check(	val_context_t	*context,
+			const char *domain_name,
+			const u_int16_t type,
+			const u_int16_t class,
+			const u_int8_t flags, 
+			struct query_chain **queries,
+			struct assertion_chain **assertions,
+			struct val_result **results);
+
+#endif /* VAL_ASSERTION_H */
