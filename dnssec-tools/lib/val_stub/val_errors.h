a4117b6f2c84edee05248299e2eec6e27c7567c1
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-06-02 21:06:38 +0000

    Error code restructuring
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@578 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/val_stub/val_errors.h
+++ dnssec-tools/lib/val_stub/val_errors.h
@@ -6,21 +6,17 @@
 #ifndef VAL_ERRORS_H
 #define VAL_ERRORS_H
 
+#include "validator.h"
+
+char *p_val_error(int valerrno);
 
 /* 
  ***********************************
- * Validator return values
+ * Return values
  ***********************************
  */
-/* Generic */
+
 #define NO_ERROR 0
-
-/* 
- ***********************************
- * Validator error values
- ***********************************
- */
-
 #define NOT_IMPLEMENTED	-1	/* Functionality not yet implemented */
 #define OUT_OF_MEMORY -2
 #define BAD_ARGUMENT -3
@@ -29,98 +25,95 @@
 #define RESOURCE_UNAVAILABLE -6	
 #define CONF_PARSE_ERROR -7 
 #define NO_POLICY -8	
+#define NO_SPACE -9
 
 #define MALFORMED_LOCALE -20
 #define UNKNOWN_LOCALE	-21
 #define FILE_ERROR	-22
 
-/* 
- ***********************************
- * Validator results
- ***********************************
- */
-
-/* Validation of an RR/message */
-#define VALIDATE_SUCCESS	 1
-#define BOGUS	 2
-#define INDETERMINATE	 3 /* 
-							* Don't know if zone is unsigned or signatures 
-							* and other records have been stripped 
-							*/
-#define PROVABLY_UNSECURE	 4
-#define SECURITY_LAME	 5 /*DNSKEY-DS mapping failure */
-#define NAME_EXPANSION_FAILURE	 6
-
 
 /* 
  ***********************************
- * Transient results
+ * Validator states 
  ***********************************
  */
 
 
+#define ERROR_BASE    			A_LAST_STATE
+/* "Cannot do anything further" states */
+#define BARE_RRSIG 				ERROR_BASE+1
+#define A_NO_DATA 				ERROR_BASE+2
+#define DNSKEY_MISSING			ERROR_BASE+3 
+#define	DS_MISSING				ERROR_BASE+4	
+#define NSEC_MISSING 			ERROR_BASE+5
+#define RRSIG_MISSING 			ERROR_BASE+6
+#define NO_TRUST_ANCHOR 		ERROR_BASE+7
+#define IRRELEVANT_DATA			ERROR_BASE+8
+#define IRRELEVANT_PROOF		ERROR_BASE+9
+#define NSEC_POINTING_UPWARDS	ERROR_BASE+10
+#define INCOMPLETE_PROOF		ERROR_BASE+11
+#define A_ERROR  				ERROR_BASE+12
+#define HEADER_ERROR			ERROR_BASE+13		
+#define EDNS_VERSION_ERROR		ERROR_BASE+14	
+#define UNSUPP_ENDS0_LABEL		ERROR_BASE+15
+#define DNSSEC_VERSION_ERROR	ERROR_BASE+16
+#define SUSPICIOUS_BIT			ERROR_BASE+17
+#define NAME_EXPANSION_FAILURE	ERROR_BASE+18
+#define TOO_MANY_LINKS	 		ERROR_BASE+19
+#define UNKNOWN_DNSKEY_PROTO  	ERROR_BASE+20
+#define FLOOD_ATTACK_DETECTED	ERROR_BASE+21	
+#define DNS_FAILURE				ERROR_BASE+22	
+#define LAST_ERROR				TOO_MANY_LINKS
 
-#define NO_PREFERRED_SEP	 7
-#define NO_TRUST_ANCHOR	 8
-#define TOO_MANY_LINKS	 9
-#define TRUST_ANCHOR_TIMEOUT	10
-/* Non-existence */
-#define OVERREACHING_NSEC	11
-#define NSEC_POINTING_UPWARDS	12
-#define IRRELEVANT_PROOF	13
-#define INCOMPLETE_PROOF	14
-#define PROVED_OWNERNAME_MISSING	15
-#define PROVED_TYPE_MISSING	16
+#define FAIL_BASE				LAST_ERROR
+/* "Error, but can prove the chain-of-trust above this" states */
+#define DNSKEY_NOMATCH			FAIL_BASE+1		
+#define WRONG_LABEL_COUNT  		FAIL_BASE+2
+#define VERIFY_PROC_ERROR	 	FAIL_BASE+3
+#define SECURITY_LAME	  		FAIL_BASE+4	/*DNSKEY-DS mapping failure */ 
+#define NOT_A_ZONE_KEY	 		FAIL_BASE+5
+#define RRSIG_NOTYETACTIVE	 	FAIL_BASE+6
+#define RRSIG_EXPIRED	 		FAIL_BASE+7
+#define ALGO_NOT_SUPPORTED	 	FAIL_BASE+8
+#define UNKNOWN_ALGO	 		FAIL_BASE+9	/* DNSKEY or RRSIG or DS */ 
+#define RRSIG_VERIFIED	 		FAIL_BASE+10
+#define RRSIG_VERIFY_FAILED		FAIL_BASE+11
+#define NOT_VERIFIED 			FAIL_BASE+12 	/* One or more failures */ 
+#define KEY_TOO_LARGE			FAIL_BASE+13 
+#define KEY_TOO_SMALL 			FAIL_BASE+14
+#define KEY_NOT_AUTHORIZED		FAIL_BASE+15
+#define ALGO_REFUSED			FAIL_BASE+16
+#define CLOCK_SKEW				FAIL_BASE+17
+#define DUPLICATE_KEYTAG		FAIL_BASE+18
+#define NO_PREFERRED_SEP		FAIL_BASE+19
+#define WRONG_RRSIG_OWNER		FAIL_BASE+20
+#define RRSIG_ALGO_MISMATCH 	FAIL_BASE+21
+#define KEYTAG_MISMATCH			FAIL_BASE+22
+#define LAST_FAILURE			NOT_VERIFIED
+
+/* Verification success */
+#define VERIFIED				LAST_FAILURE+1 
+
+/* Validation result states */
+#define VALIDATION_RESULT_BASE	VERIFIED
+#define BOGUS  					VALIDATION_RESULT_BASE+1	/* NOT_VERIFIED but not trusted */
+#define PROVABLY_UNSECURE  		VALIDATION_RESULT_BASE+2	/* TRUSTED AND NEGATIVE_PROOF */
+#define NONEXISTENT  			VALIDATION_RESULT_BASE+3	/* TRUSTED AND proof present */
+#define VALIDATE_SUCCESS  		VALIDATION_RESULT_BASE+4	/* TRUSTED AND no error */
+#define INDETERMINATE  			VALIDATION_RESULT_BASE+5	/* Can't say */
+#define A_LOCAL 				VALIDATION_RESULT_BASE+6	/* Answer obtained locally */
 
 
-
-/* Verification of signatures */
-#define RRSIG_VERIFIED	17
-#define RRSIG_VERIFY_FAILED	18
-#define BARE_RRSIG	19
-#define RRSIG_EXPIRED	20
-#define RRSIG_NOTYETACTIVE	21
-#define KEY_TOO_LARGE 22
-#define KEY_TOO_SMALL 23
-#define KEY_NOT_AUTHORIZED 24
-#define NOT_A_ZONE_KEY	25
-#define CLOCK_SKEW		26 
-#define ALGO_REFUSED	27
+/*
+// need more info about when these conditions can occur
+#define OVERREACHING_NSEC	XX
 #define UNAUTHORIZED_SIGNER	28
-
-#define RRSIG_MISSING	29
-#define DNSKEY_MISSING	30
-#define	DS_MISSING	31
-#define NSEC_MISSING 32
-#define DUPLICATE_KEYTAG	33
-#define CONFLICTING_PROOFS	34 /* Two validated NSECs describe different things */
-
-#define UNKNOWN_ALGO	35 /* DNSKEY or RRSIG or DS */
-#define ALGO_NOT_SUPPORTED	36
-#define WRONG_RRSIG_OWNER	37
-#define KEYTAG_MISMATCH	38
-#define UNKNOWN_DNSKEY_PROTO 39
-#define DNS_FAILURE	40
+#define CONFLICTING_PROOFS	34 
+#define TRUST_ANCHOR_TIMEOUT	XX	
 #define WAITING 41
 #define WAKEUP 42
-#define FLOOD_ATTACK_DETECTED	43	
-
-
 #define INSUFFICIENT_DATA	XX
+*/
 
-
-/* 
- ***********************************
- * Parsing messages
- ***********************************
- */
-#define HEADER_ERROR	XX // Generic header error
-#define WRONG_LABEL_COUNT	XX
-#define EDNS_VERSION_ERROR	XX
-#define UNSUPP_ENDS0_LABEL	XX
-#define DNSSEC_VERSION_ERROR	XX 
-#define SUSPICIOUS_BIT	XX  // Information hiding?
-
-char *p_val_error(int valerrno);
 
 #endif /* VAL_ERRORS_H */
