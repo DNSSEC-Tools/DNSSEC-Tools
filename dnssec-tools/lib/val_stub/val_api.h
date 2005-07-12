2e45a903cd783de2b334b1f9c1e873a669026547
ahayatnagarkar <ahayatnagarkar@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-07-12 20:10:01 +0000

    Moved some declarations from val_api to val_gethostbyname.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@625 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/val_stub/val_api.h
+++ dnssec-tools/lib/val_stub/val_api.h
@@ -14,53 +14,18 @@
 #include <netdb.h>       /* for struct hostent */
 
 #include "val_errors.h"
-
-/* A macro to free memory allocated by val_gethostbyname */
-#define FREE_HOSTENT(hentry) do { \
-	if (hentry) { \
-	    int i = 0; \
-	    if (hentry->h_name) free (hentry->h_name); \
-	    if (hentry->h_aliases) { \
-                i = 0; \
-		for (i=0; hentry->h_aliases[i] != 0; i++) { \
-		    if (hentry->h_aliases[i]) free (hentry->h_aliases[i]); \
-		} \
-		if (hentry->h_aliases[i]) free (hentry->h_aliases[i]); \
-		free (hentry->h_aliases); \
-	    } \
-	    if (hentry->h_addr_list) { \
-                i = 0; \
-		for (i=0; hentry->h_addr_list[i] != 0; i++) { \
-		    if (hentry->h_addr_list[i]) free (hentry->h_addr_list[i]); \
-		} \
-		if (hentry->h_addr_list[i]) free (hentry->h_addr_list[i]); \
-		free (hentry->h_addr_list); \
-	    } \
-	    free (hentry); \
-	} \
-} while (0);
-
-/* Possible values for val_h_errno are similar to those of
- * h_errno in netdb.h */
-int val_h_errno;
+#include "val_gethostbyname.h"
+#include "val_getaddrinfo.h"
 
 /*
  * Returns the length (in bytes) of the answer on success, and -1 on
- * failure.  If successful, *dnssec_status will contain VALIDATE_SUCCESS
+ * failure.  If DNSSEC validation is successful, *dnssec_status will
+ * contain VALIDATE_SUCCESS
  * If there is a failure, *dnssec_status will contain the validator
  * error code.
  */
 int val_query ( const char *domain_name, int class, int type,
 		unsigned char *answer, int anslen,
 		int *dnssec_status );
-
-/*
- * Returns the entry from the hosts file and DNS for host with name.
- * If successful, *dnssec_status will contain VALIDATE_SUCCESS
- * If there is a failure, *dnssec_status will contain the validator
- * error code.  Applications can use the FREE_HOSTENT() macro given above
- * to free the returned hostent structure.
- */
-struct hostent *val_gethostbyname ( const char *name, int *dnssec_status );
 
 #endif
