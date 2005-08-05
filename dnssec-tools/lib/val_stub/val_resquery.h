13cca16b82c0ec43e10fd15c0dc2ea123a911d07
Suresh Krishnaswamy <hserus>
2005-08-05 20:31:37 +0000

    Re-named res* files to val* files

--- dnssec-tools/lib/val_stub/val_resquery.h
+++ dnssec-tools/lib/val_stub/val_resquery.h
@@ -2,16 +2,16 @@
  * Copyright 2005 SPARTA, Inc.  All rights reserved.
  * See the COPYING file distributed with this software for details.
  */ 
-#ifndef RES_SQUERY_H
-#define RES_SQUERY_H
+#ifndef VAL_RESQUERY_H
+#define VAL_RESQUERY_H
 
 #include "validator.h"
 
-int res_squery ( 	val_context_t			*context,
+int val_resquery ( 	val_context_t			*context,
 					const char              *domain_name,
                     const u_int16_t         type,
                     const u_int16_t         class,
 					struct res_policy 		*respol, 
 					struct domain_info      *response);
 
-#endif /* RES_SQUERY_H */
+#endif /* VAL_RESQUERY_H */
