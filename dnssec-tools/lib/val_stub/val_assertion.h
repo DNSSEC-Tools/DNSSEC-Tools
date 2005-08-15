0b89b9cf3956e7eeca650ca1601dcd0c84547a35
Suresh Krishnaswamy <hserus>
2005-08-15 21:14:32 +0000

    Remove prototypes for support calls moved from val_assertion.c to val_support.c

--- dnssec-tools/lib/val_stub/val_assertion.h
+++ dnssec-tools/lib/val_stub/val_assertion.h
@@ -13,16 +13,12 @@ struct val_result {
 	struct val_result *next;
 };
 
-void lower_name (u_int8_t rdata[], int *index);
 int assimilate_answers(val_context_t *context, struct query_chain **queries, 
 							struct domain_info *response, struct query_chain *matched_q, 
 								struct assertion_chain **assertions);
-int add_to_query_chain(struct query_chain **queries, u_char *name_n, 
-						const u_int16_t type_h, const u_int16_t class_h);
 int add_to_assertion_chain(struct assertion_chain **assertions, struct rrset_rec *response_data);
 void free_assertion_chain(struct assertion_chain **assertions);
 void free_result_chain(struct val_result **results);
-void free_query_chain(struct query_chain **queries);
 
 int resolve_n_check(	val_context_t	*context,
 			const char *domain_name,
