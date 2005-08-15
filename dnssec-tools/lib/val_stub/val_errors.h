2a613c21414de8d3af78f54f04d747e4eda54d12
Suresh Krishnaswamy <hserus>
2005-08-15 21:15:34 +0000

    Clean and Re-organize error codes yet again. Major change here is that DNS (resolver) error codes are also made available.

--- dnssec-tools/lib/val_stub/val_errors.h
+++ dnssec-tools/lib/val_stub/val_errors.h
@@ -10,37 +10,36 @@
 
 char *p_val_error(int valerrno);
 
-/* 
- ***********************************
- * Return values
- ***********************************
- */
-
 #define NO_ERROR 0
-#define NOT_IMPLEMENTED	-1	/* Functionality not yet implemented */
-#define OUT_OF_MEMORY -2
-#define BAD_ARGUMENT -3
-#define INTERNAL_ERROR -4
-#define NO_PERMISSION	-5
-#define RESOURCE_UNAVAILABLE -6	
-#define CONF_PARSE_ERROR -7 
-#define NO_POLICY -8	
-#define NO_SPACE -9
-#define CONTEXT_ERROR -10
+
+/*
+ *************************************************** 
+ * Process error codes 
+ *************************************************** 
+ */
+#define ERROR	-1 /* Generic error */
+#define NOT_IMPLEMENTED	-2	/* Functionality not yet implemented */
+#define OUT_OF_MEMORY -3
+#define BAD_ARGUMENT -4
+#define INTERNAL_ERROR -5
+#define NO_PERMISSION	-6
+#define RESOURCE_UNAVAILABLE -7	
+#define CONF_PARSE_ERROR -8 
+#define NO_POLICY -9	
+#define NO_SPACE -10
+#define CONTEXT_ERROR -11
 
 #define MALFORMED_LOCALE -20
 #define UNKNOWN_LOCALE	-21
 #define FILE_ERROR	-22
 
-
 /* 
- ***********************************
- * Validator states 
- ***********************************
+ *************************************************** 
+ * Validator return codes 
+ *************************************************** 
  */
 
-
-#define ERROR_BASE    			A_LAST_STATE
+#define ERROR_BASE    			A_LAST_STATE /* 10 */
 /* "Cannot do anything further" states */
 #define BARE_RRSIG 				ERROR_BASE+1
 #define A_NO_DATA 				ERROR_BASE+2
@@ -49,23 +48,27 @@ char *p_val_error(int valerrno);
 #define NSEC_MISSING 			ERROR_BASE+5
 #define RRSIG_MISSING 			ERROR_BASE+6
 #define NO_TRUST_ANCHOR 		ERROR_BASE+7
-#define IRRELEVANT_DATA			ERROR_BASE+8
-#define IRRELEVANT_PROOF		ERROR_BASE+9
-#define A_ERROR  				ERROR_BASE+10
-#define HEADER_ERROR			ERROR_BASE+11		
-#define EDNS_VERSION_ERROR		ERROR_BASE+12	
-#define UNSUPP_ENDS0_LABEL		ERROR_BASE+13
-#define DNSSEC_VERSION_ERROR	ERROR_BASE+14
-#define SUSPICIOUS_BIT			ERROR_BASE+15
-#define NAME_EXPANSION_FAILURE	ERROR_BASE+16
-#define TOO_MANY_LINKS	 		ERROR_BASE+17
-#define UNKNOWN_DNSKEY_PROTO  	ERROR_BASE+18
-#define FLOOD_ATTACK_DETECTED	ERROR_BASE+19	
-#define DNS_FAILURE				ERROR_BASE+20	
-#define LAST_ERROR				DNS_FAILURE
+#define IRRELEVANT_PROOF		ERROR_BASE+8
+#define A_ERROR  				ERROR_BASE+9 
+#define HEADER_ERROR			ERROR_BASE+10		
+#define EDNS_VERSION_ERROR		ERROR_BASE+11	
+#define UNSUPP_ENDS0_LABEL		ERROR_BASE+12
+#define DNSSEC_VERSION_ERROR	ERROR_BASE+13
+#define SUSPICIOUS_BIT			ERROR_BASE+14
+#define NAME_EXPANSION_FAILURE	ERROR_BASE+15
+#define TOO_MANY_LINKS	 		ERROR_BASE+16
+#define UNKNOWN_DNSKEY_PROTO  	ERROR_BASE+17
+#define FLOOD_ATTACK_DETECTED	ERROR_BASE+18	
 
-#define FAIL_BASE				LAST_ERROR
+#define DNS_ERROR_BASE			ERROR_BASE+20
+/* 
+ * DNS errors lie within this range, 
+ * there are SR_LAST_ERROR of them in total
+ */
+#define LAST_ERROR				DNS_ERROR_BASE+SR_LAST_ERROR 
+
 /* "Error, but can prove the chain-of-trust above this" states */
+#define FAIL_BASE				LAST_ERROR /* ERROR_BASE+40 */ 
 #define DNSKEY_NOMATCH			FAIL_BASE+1		
 #define WRONG_LABEL_COUNT  		FAIL_BASE+2
 #define VERIFY_PROC_ERROR	 	FAIL_BASE+3
@@ -88,13 +91,13 @@ char *p_val_error(int valerrno);
 #define WRONG_RRSIG_OWNER		FAIL_BASE+20
 #define RRSIG_ALGO_MISMATCH 	FAIL_BASE+21
 #define KEYTAG_MISMATCH			FAIL_BASE+22
-#define LAST_FAILURE			KEYTAG_MISMATCH
+#define LAST_FAILURE			FAIL_BASE+30 /* ERROR_BASE + 70 */
 
 /* success results conditions */
 #define VERIFIED				LAST_FAILURE+1 
 #define VALIDATE_SUCCESS  		LAST_FAILURE+2	/* TRUSTED AND no error */
 #define A_LOCAL 				LAST_FAILURE+3	/* Answer obtained locally */
-#define LAST_SUCCESS			A_LOCAL
+#define LAST_SUCCESS			LAST_FAILURE+10 /* ERROR_BASE + 80 */
 
 /* failure result conditions */
 #define BOGUS  					LAST_SUCCESS+1	/* NOT_VERIFIED but not trusted */
@@ -109,18 +112,6 @@ char *p_val_error(int valerrno);
 #define INDETERMINATE_TRUST		LAST_SUCCESS+8	/* Dont know if trust is absent or answer is bogus */
 #define INDETERMINATE_ZONE		LAST_SUCCESS+9	/* Dont know if zone is unsigned or sigs have been stripped */
 #define INDETERMINATE	INDETERMINATE_TRUST
-
-
-/*
-// need more info about when these conditions can occur
-#define OVERREACHING_NSEC	XX
-#define UNAUTHORIZED_SIGNER	28
-#define CONFLICTING_PROOFS	34 
-#define TRUST_ANCHOR_TIMEOUT	XX	
-#define WAITING 41
-#define WAKEUP 42
-#define INSUFFICIENT_DATA	XX
-*/
 
 
 #endif /* VAL_ERRORS_H */
