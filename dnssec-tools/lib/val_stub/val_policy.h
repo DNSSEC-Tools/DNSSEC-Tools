8fafb90e835e48e0c69973c86d2d6120c2f944e1
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-08-16 18:35:26 +0000

    Create definitions for all the keywords used in the validator configuration file instead of using hardcoded values. No longer defining policy for not_preferred_* cases. Defined a new policy type for storing the zone security status.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@761 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/val_stub/val_policy.h
+++ dnssec-tools/lib/val_stub/val_policy.h
@@ -19,6 +19,26 @@
 	}												\
 } while (0)
 
+#define POL_TRUST_ANCHOR_STR "trust-anchor"
+#define POL_PREFERRED_SEP_STR "preferred-sep"
+#define POL_MUST_VERIFY_COUNT_STR "must-verify-count"
+#define POL_PREFERRED_ALGO_DATA_STR "preferred-algo-data"
+#define POL_PREFERRED_ALGO_KEYS_STR "preferred-algo-keys"
+#define POL_PREFERRED_ALGO_DS_STR "preferred-algo-ds"
+#define POL_CLOCK_SKEW_STR "clock-skew"
+#define POL_EXPIRED_SIGS_STR "expired-sigs"
+#define POL_USE_TCP_STR "use-tcp"
+#define POL_ZONE_SE_STR "zone-security-expectation"
+#ifdef DLV
+#define POL_DLV_TRUST_POINTS_STR  "dlv-trust-points"
+#define POL_DLV_MAX_LINKS_STR "dlv-max-links"
+#endif
+
+#define ZONE_SE_YES "trusted"
+#define ZONE_SE_NO "untrusted"
+
+
+
 int read_res_config_file(val_context_t *ctx);
 int read_val_config_file(val_context_t *ctx, const char *scope);
 void destroy_valpol(val_context_t *ctx);
@@ -29,28 +49,22 @@ int parse_trust_anchor(FILE*, policy_entry_t*, int*);
 int free_trust_anchor(policy_entry_t*);
 int parse_preferred_sep(FILE*, policy_entry_t*, int*);
 int free_preferred_sep(policy_entry_t*);
-int parse_not_preferred_sep(FILE*, policy_entry_t*, int*);
-int free_not_preferred_sep(policy_entry_t*);
 int parse_must_verify_count(FILE*, policy_entry_t*, int*);
 int free_must_verify_count(policy_entry_t*);
 int parse_preferred_algo_data(FILE*, policy_entry_t*, int*);
 int free_preferred_algo_data(policy_entry_t*);
-int parse_not_preferred_algo_data(FILE*, policy_entry_t*, int*);
-int free_not_preferred_algo_data(policy_entry_t*);
 int parse_preferred_algo_keys(FILE*, policy_entry_t*, int*);
 int free_preferred_algo_keys(policy_entry_t*);
-int parse_not_preferred_algo_keys(FILE*, policy_entry_t*, int*);
-int free_not_preferred_algo_keys(policy_entry_t*);
 int parse_preferred_algo_ds(FILE*, policy_entry_t*, int*);
 int free_preferred_algo_ds(policy_entry_t*);
-int parse_not_preferred_algo_ds(FILE*, policy_entry_t*, int*);
-int free_not_preferred_algo_ds(policy_entry_t*);
 int parse_clock_skew(FILE*, policy_entry_t*, int*);
 int free_clock_skew(policy_entry_t*);
 int parse_expired_sigs(FILE*, policy_entry_t*, int*);
 int free_expired_sigs(policy_entry_t*);
 int parse_use_tcp(FILE*, policy_entry_t*, int*);
 int free_use_tcp(policy_entry_t*);
+int parse_zone_security_expectation(FILE*, policy_entry_t*, int*);
+int free_zone_security_expectation(policy_entry_t*);
 #ifdef DLV
 int parse_dlv_trust_points(FILE*, policy_entry_t*, int*);
 int free_dlv_trust_points(policy_entry_t*);
@@ -80,5 +94,11 @@ struct trust_anchor_policy {
 	val_dnskey_rdata_t *publickey;
 	struct trust_anchor_policy *next;
 }; 
+
+struct zone_se_policy {
+	u_int8_t zone_n[MAXCDNAME];
+	int trusted;
+	struct zone_se_policy *next;
+};
 
 #endif /* VAL_POLICY_H */ 
