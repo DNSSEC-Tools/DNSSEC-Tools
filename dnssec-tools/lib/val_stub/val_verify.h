ba5038b840a95a653912a425c5d4d0c9673174d8
Abhijit Hayatnagarkar <ahayatnagarkar>
2005-08-18 16:55:36 +0000

    Removed the val_verify method.

--- dnssec-tools/lib/val_stub/val_verify.h
+++ dnssec-tools/lib/val_stub/val_verify.h
@@ -21,14 +21,7 @@ typedef int val_result_t;
 
 /*
  * The Verifier Function.
- *
- * Returns VALIDATE_SUCCESS if DNSSEC validation succeeds,
- *         other error codes DNSSEC validation fails
- *         and other values as given above.
- * It takes in a domain_info struct, and does not make any resolver queries itself.
- * It's just a passive verifier.
  */
-val_result_t val_verify (struct val_context *context, struct domain_info *response);
 void verify_next_assertion(struct assertion_chain *as);
 
 #endif
