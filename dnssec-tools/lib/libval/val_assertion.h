7a2d9c2136c26234fb85d7669745ae33ae486583
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-08-23 16:46:45 +0000

    Moved add_to_query_chain(), free_query_chain() from val_support.c to val_assertion.c since the latter is the only file where these methods are used. Moved the val_result structure to validator.h since this is part of the libval API
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@901 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libval/val_assertion.h
+++ dnssec-tools/lib/libval/val_assertion.h
@@ -6,20 +6,16 @@
 #ifndef VAL_ASSERTION_H
 #define VAL_ASSERTION_H
 
-struct val_result {
-	struct assertion_chain *as;
-	int status;
-	int trusted;
-	struct val_result *next;
-};
 
 int assimilate_answers(val_context_t *context, struct query_chain **queries, 
 							struct domain_info *response, struct query_chain *matched_q, 
 								struct assertion_chain **assertions);
+int add_to_query_chain(struct query_chain **queries, u_char *name_n, 
+						const u_int16_t type_h, const u_int16_t class_h);
 int add_to_assertion_chain(struct assertion_chain **assertions, struct rrset_rec *response_data);
+void free_query_chain(struct query_chain **queries);
 void free_assertion_chain(struct assertion_chain **assertions);
 void free_result_chain(struct val_result **results);
-
 int resolve_n_check(	val_context_t	*context,
 			u_char *domain_name_n,
 			const u_int16_t type,
