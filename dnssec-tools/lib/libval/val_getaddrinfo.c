53912994cea31d77a664bd0e6504a1546f782f32
Suresh Krishnaswamy <hserus>
2005-08-23 15:46:09 +0000

    resolve_n_check() invokes add_to_query_chain() first. First parameter to resolve_n_check() is now of type u_char *.

--- dnssec-tools/lib/libval/val_getaddrinfo.c
+++ dnssec-tools/lib/libval/val_getaddrinfo.c
@@ -425,14 +425,9 @@ static int get_addrinfo_from_dns (val_context_t *ctx,
 		val_log("checking for A records\n");
 		
 		if ((retval = ns_name_pton(nodename, name_n, MAXCDNAME - 1)) != -1) {
-			if ((retval = add_to_query_chain(&queries, name_n, ns_t_a, ns_c_in)) == NO_ERROR) {
-				if ((retval = resolve_n_check(context, name_n, ns_t_a, ns_c_in, 0,
-							      &queries, &assertions, &results)) != NO_ERROR) {
-					val_log("resolve_n_check failed");
-				}
-			}
-			else {
-				val_log("add_to_query_chain failed");
+			if ((retval = resolve_n_check(context, name_n, ns_t_a, ns_c_in, 0,
+						      &queries, &assertions, &results)) != NO_ERROR) {
+				val_log("resolve_n_check failed");
 			}
 		}
 		else {
