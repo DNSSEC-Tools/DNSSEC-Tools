010f5fea45bafc3d8071960a8ce5c1423da8ba62
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-08-23 22:49:15 +0000

    Added descriptions for error codes. Added definition for BOGUS_PROVABLE. Removed the CONTEXT_ERROR code since the actual policy configuration errors are now propagated to the application. Also removed MALFORMED_LOCALE and VERIFY_PROC_ERROR definitions.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@919 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libval/val_errors.h
+++ dnssec-tools/lib/libval/val_errors.h
@@ -17,19 +17,15 @@
  */
 #define ERROR	-1 /* Generic error */
 #define NOT_IMPLEMENTED	-2	/* Functionality not yet implemented */
-#define OUT_OF_MEMORY -3
-#define BAD_ARGUMENT -4
-#define INTERNAL_ERROR -5
-#define NO_PERMISSION	-6
-#define RESOURCE_UNAVAILABLE -7	
-#define CONF_PARSE_ERROR -8 
-#define NO_POLICY -9	
-#define NO_SPACE -10
-#define CONTEXT_ERROR -11
-
-#define MALFORMED_LOCALE -20
-#define UNKNOWN_LOCALE	-21
-#define FILE_ERROR	-22
+#define OUT_OF_MEMORY -3 /*Could not allocate memory.*/
+#define BAD_ARGUMENT -4 /*Bad arguments passed as parameters.*/
+#define INTERNAL_ERROR -5 /*Encountered some internal error.*/
+#define NO_PERMISSION	-6 /*No permission to perform operation.*/
+#define RESOURCE_UNAVAILABLE -7	/*Some resource (crypto possibly) was unavailable.*/
+#define CONF_PARSE_ERROR -8 /*Error in parsing some configuration file.*/ 
+#define NO_POLICY -9	/*Could not find one or both of the configuration files*/
+#define NO_SPACE -10 /*Not enough space for storing all available answers.*/
+#define UNKNOWN_LOCALE -11 /*Could not identify the policy to which we need to switch.*/	
 
 /* 
  *************************************************** 
@@ -38,8 +34,8 @@
  */
 #define NO_ERROR 0
 
-#define ERROR_BASE    			A_LAST_STATE /* 10 */
 /* "Cannot do anything further" states */
+#define ERROR_BASE    			A_LAST_STATE /* 10 */
 #define DATA_MISSING			ERROR_BASE+2
 #define DNSKEY_MISSING			ERROR_BASE+3 
 #define	DS_MISSING				ERROR_BASE+4	
@@ -69,53 +65,53 @@
 
 /* "Error, but can prove the chain-of-trust above this" states */
 #define FAIL_BASE				LAST_ERROR /* ERROR_BASE+40 */ 
-#define DNSKEY_NOMATCH			FAIL_BASE+1		
-#define WRONG_LABEL_COUNT  		FAIL_BASE+2
-#define VERIFY_PROC_ERROR	 	FAIL_BASE+3
-#define SECURITY_LAME	  		FAIL_BASE+4	/*DNSKEY-DS mapping failure */ 
-#define NOT_A_ZONE_KEY	 		FAIL_BASE+5
-#define RRSIG_NOTYETACTIVE	 	FAIL_BASE+6
-#define RRSIG_EXPIRED	 		FAIL_BASE+7
-#define ALGO_NOT_SUPPORTED	 	FAIL_BASE+8
-#define UNKNOWN_ALGO	 		FAIL_BASE+9	/* DNSKEY or RRSIG or DS */ 
-#define RRSIG_VERIFIED	 		FAIL_BASE+10
-#define RRSIG_VERIFY_FAILED		FAIL_BASE+11
-#define NOT_VERIFIED 			FAIL_BASE+12 	/* One or more failures */ 
-#define KEY_TOO_LARGE			FAIL_BASE+13 
-#define KEY_TOO_SMALL 			FAIL_BASE+14
-#define KEY_NOT_AUTHORIZED		FAIL_BASE+15
-#define ALGO_REFUSED			FAIL_BASE+16
-#define CLOCK_SKEW				FAIL_BASE+17
-#define DUPLICATE_KEYTAG		FAIL_BASE+18
-#define NO_PREFERRED_SEP		FAIL_BASE+19
-#define WRONG_RRSIG_OWNER		FAIL_BASE+20
-#define RRSIG_ALGO_MISMATCH 	FAIL_BASE+21
-#define KEYTAG_MISMATCH			FAIL_BASE+22
+#define DNSKEY_NOMATCH			FAIL_BASE+1 /*RRSIG was created by a DNSKEY that does not exist in the apex keyset.*/
+#define WRONG_LABEL_COUNT  		FAIL_BASE+2 /*The number of labels on the signature is greater than the the count given in the RRSIG RDATA.*/
+#define SECURITY_LAME	  		FAIL_BASE+3 /*RRSIG created by a key that does not exist in the parent DS record set.*/
+#define NOT_A_ZONE_KEY	 		FAIL_BASE+4 /*The key used to verify the RRSIG is not a zone key, but some other key such as the public key used for TSIG.*/
+#define RRSIG_NOTYETACTIVE	 	FAIL_BASE+5 /*The RRSIG's inception time is in the future.*/
+#define RRSIG_EXPIRED	 		FAIL_BASE+6 /*The RRSIG has expired.*/
+#define ALGO_NOT_SUPPORTED	 	FAIL_BASE+7 /* Algorithm in DNSKEY or RRSIG or DS is not supported.*/
+#define UNKNOWN_ALGO	 		FAIL_BASE+8	/* Unknown DNSKEY or RRSIG or DS algorithm */ 
+#define RRSIG_VERIFIED	 		FAIL_BASE+9  /* The RRSIG verified successfully.*/ 
+#define RRSIG_VERIFY_FAILED		FAIL_BASE+10 /*The RRSIG did not verify.*/ 
+#define NOT_VERIFIED 			FAIL_BASE+11 /*Different RRSIGs failed for different reasons */ 
+#define KEY_TOO_LARGE			FAIL_BASE+12 /*The zone is using a key size that is too large as per local policy.*/
+#define KEY_TOO_SMALL 			FAIL_BASE+13 /*The zone is using a key size that is too small as per local policy*/
+#define KEY_NOT_AUTHORIZED		FAIL_BASE+14 /*The zone is using a key that is not authorized as per local policy.*/
+#define ALGO_REFUSED			FAIL_BASE+15 /*Algorithm in DNSKEY or RRSIG or DS is not allowed as per local policy */
+#define CLOCK_SKEW				FAIL_BASE+16 /*Verified but with clock skew taken into account*/
+#define DUPLICATE_KEYTAG		FAIL_BASE+17 /*Two DNSKEYs have the same keytag*/
+#define NO_PREFERRED_SEP		FAIL_BASE+18 /*There is no DNSKEY in the parent DS set that our local policy allows us to traverse*/
+#define WRONG_RRSIG_OWNER		FAIL_BASE+19 /* The RRSIG and the data that it purportedly covers have differing notions of owner name*/
+#define RRSIG_ALGO_MISMATCH 	FAIL_BASE+20 /* The DNSKEY and RRSIG pair have a mismatch in their algorithm.*/
+#define KEYTAG_MISMATCH			FAIL_BASE+21 /* The DNSKEY and RRSIG pair have a mismatch in their key tags*/
 #define LAST_FAILURE			FAIL_BASE+30 /* ERROR_BASE + 70 */
 
 /* success results conditions */
-#define VERIFIED				LAST_FAILURE+1 
+#define VERIFIED				LAST_FAILURE+1 /* This is a transient state, it will settle at VALIDATED_SUCCESS if the
+chain of trust can be completed */ 
 #define LOCAL_ANSWER			LAST_FAILURE+2	/* Answer obtained locally */
-#define TRUST_KEY	 			LAST_FAILURE+3 
-#define TRUST_ZONE				LAST_FAILURE+4 
-#define BARE_RRSIG 				LAST_FAILURE+5
+#define TRUST_KEY	 			LAST_FAILURE+3 /* key is trusted */ 
+#define TRUST_ZONE				LAST_FAILURE+4 /* zone is trusted */
+#define BARE_RRSIG 				LAST_FAILURE+5 /* No DNSSEC validation possible, query was for a RRSIG.*/
 #define VALIDATE_SUCCESS  		LAST_FAILURE+6	/* TRUSTED AND no error */
 #define LAST_SUCCESS			LAST_FAILURE+10 /* ERROR_BASE + 80 */
 
 /* failure result conditions */
-#define BOGUS  					LAST_SUCCESS+1	/* NOT_VERIFIED but not trusted */
-#define VALIDATION_ERROR  		LAST_SUCCESS+2	
-#define NONEXISTENT_NAME 		LAST_SUCCESS+3	/* TRUSTED AND proof present */
-#define NONEXISTENT_TYPE 		LAST_SUCCESS+4	/* TRUSTED AND proof present */
-#define INCOMPLETE_PROOF 		LAST_SUCCESS+5	/* Proof does not have all required components */
-#define BOGUS_PROOF 			LAST_SUCCESS+6	/* proof cannot be validated */
-#define INDETERMINATE_DS 		LAST_SUCCESS+7	/* Can't prove that the DS is trusted */
-#define INDETERMINATE_PROOF		LAST_SUCCESS+8	/* Some intermediate Proof of non-existence obtained 
-													- dont know if answer exists and proof is bogus
-													  or answer is bogus.  */
-#define INDETERMINATE_ERROR		LAST_SUCCESS+9	/* Error sequence */
-#define INDETERMINATE_TRUST		LAST_SUCCESS+10 /* Dont know if trust is absent or answer is bogus */
-#define INDETERMINATE_ZONE		LAST_SUCCESS+11 /* Dont know if zone is unsigned or sigs have been stripped */
+#define BOGUS_PROVABLE			LAST_SUCCESS+1	/* NOT_VERIFIED but not trusted */
+#define BOGUS_UNPROVABLE		LAST_SUCCESS+2	/* NOT_VERIFIED but not trusted */
+#define BOGUS BOGUS_UNPROVABLE
+#define VALIDATION_ERROR  		LAST_SUCCESS+3	
+#define NONEXISTENT_NAME 		LAST_SUCCESS+4	/* TRUSTED AND proof present */
+#define NONEXISTENT_TYPE 		LAST_SUCCESS+5	/* TRUSTED AND proof present */
+#define INCOMPLETE_PROOF 		LAST_SUCCESS+6	/* Proof does not have all required components */
+#define BOGUS_PROOF 			LAST_SUCCESS+7	/* proof cannot be validated */
+#define INDETERMINATE_DS 		LAST_SUCCESS+8	/* Can't prove that the DS is trusted */
+#define INDETERMINATE_PROOF		LAST_SUCCESS+9	/* Some intermediate Proof of non-existence obtained - dont know if answer exists and proof is bogus or answer is bogus.  */
+#define INDETERMINATE_ERROR		LAST_SUCCESS+10 /* Error sequence */
+#define INDETERMINATE_TRUST		LAST_SUCCESS+11 /* Dont know if trust is absent or answer is bogus */
+#define INDETERMINATE_ZONE		LAST_SUCCESS+12 /* Dont know if zone is unsigned or sigs have been stripped */
 #define INDETERMINATE	INDETERMINATE_TRUST
 
 
