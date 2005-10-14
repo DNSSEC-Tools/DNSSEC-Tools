eef2c3d01de03159a52a4d5a16b30f3cb0e68716
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-10-14 17:30:25 +0000

    Added prototypes for functions that print the details of a given name server and assertion status information.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1050 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libval/val_print.h
+++ dnssec-tools/lib/libval/val_print.h
@@ -12,5 +12,7 @@ void dump_val_context(struct val_context *context);
 void val_print_base64(unsigned char * message, int message_len);
 void val_print_rrsig_rdata (const char *prefix, val_rrsig_rdata_t *rdata);
 void val_print_dnskey_rdata (const char *prefix, val_dnskey_rdata_t *rdata);
+char *print_ns_string(struct name_server **server);
+void val_print_assertion_chain(struct val_result *results, struct query_chain *queries);
 
 #endif
