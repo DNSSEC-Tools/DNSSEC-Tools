2f91e470a2a869aa163dbbf3a90fa6d7c40b83ba
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-11-02 17:29:22 +0000

    Added prototype definitions for modified and new functions.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1070 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libval/val_cache.h
+++ dnssec-tools/lib/libval/val_cache.h
@@ -5,14 +5,12 @@
 #ifndef RES_CACHE_H
 #define RES_CACHE_H
 
-void stow_zone_info(struct rrset_rec *new_info);
-void stow_key_info(struct rrset_rec *new_info);
-void stow_ds_info(struct rrset_rec *new_info);
-void stow_answer(struct rrset_rec *new_info);
-struct rrset_rec* get_cached_zones();
-struct rrset_rec* get_cached_keys();
-struct rrset_rec* get_cached_ds();
-struct rrset_rec* get_cached_answers();
-void free_validator_cache();
+int stow_zone_info(struct rrset_rec *new_info);
+int stow_key_info(struct rrset_rec *new_info);
+int stow_ds_info(struct rrset_rec *new_info);
+int stow_answer(struct rrset_rec *new_info);
+int get_cached_rrset(u_int8_t *name_n, u_int16_t class_h, 
+		u_int16_t type_h, struct rrset_rec **cloned_answer);
+int free_validator_cache();
 
 #endif
