1ea55ac7c867eb328e40b7d260180f008ff293ea
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-11-10 15:50:16 +0000

    Do not list prototypes of functions that are static
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1106 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libval/val_assertion.h
+++ dnssec-tools/lib/libval/val_assertion.h
@@ -6,13 +6,6 @@
 #ifndef VAL_ASSERTION_H
 #define VAL_ASSERTION_H
 
-
-int assimilate_answers(val_context_t *context, struct query_chain **queries, 
-							struct domain_info *response, struct query_chain *matched_q, 
-								struct assertion_chain **assertions);
-int add_to_query_chain(struct query_chain **queries, u_char *name_n, 
-						const u_int16_t type_h, const u_int16_t class_h);
-int add_to_assertion_chain(struct assertion_chain **assertions, struct rrset_rec *response_data);
 void free_query_chain(struct query_chain **queries);
 void free_assertion_chain(struct assertion_chain **assertions);
 void free_result_chain(struct val_result **results);
