e3ec20ff2a74c357982c4006908ffd28182de6f7
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-11-15 00:53:49 +0000

    created a README file
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1122 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/tools/modules/ZoneFile-Fast/Fast.pm
+++ dnssec-tools/tools/modules/ZoneFile-Fast/Fast.pm
@@ -4,6 +4,34 @@
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return.   Anton Berezin
 # ----------------------------------------------------------------------------
+# Copyright (c) 2005, SPARTA, Inc.
+# All rights reserved.
+#  
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#  
+# *  Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#  
+# *  Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  
+# *  Neither the name of SPARTA, Inc nor the names of its contributors may
+#    be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#  
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # $Id$
 #
@@ -17,7 +45,7 @@ use IO::File;
 use Net::DNS;
 use MIME::Base64;
 
-$VERSION = '0.5';
+$VERSION = '0.6';
 
 my $MAXIMUM_TTL = 0x7fffffff;
 
@@ -1150,22 +1178,57 @@ thing on around 50000 real life zones I tested it with.
 
 Copyright 2003 by Anton Berezin and catpipe Systems ApS
 
- "THE BEER-WARE LICENSE" (Revision 42)
- <tobez@tobez.org> wrote this module.  As long as you retain this notice
- you can do whatever you want with this stuff. If we meet some day, and
- you think this stuff is worth it, you can buy me a beer in return.
+  "THE BEER-WARE LICENSE" (Revision 42)
+  <tobez@tobez.org> wrote this module.  As long as you retain this notice
+  you can do whatever you want with this stuff. If we meet some day, and
+  you think this stuff is worth it, you can buy me a beer in return.
 
- Anton Berezin
+  Anton Berezin
+
+Copyright (c) 2004-2005, SPARTA, Inc.
+  All rights reserved.
+   
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+   
+  *  Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+   
+  *  Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+   
+  *  Neither the name of SPARTA, Inc nor the names of its contributors may
+     be used to endorse or promote products derived from this software
+     without specific prior written permission.
+   
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+  IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =head1 CREDITS
 
-This module was largely inspired by the I<Net::DNS::ZoneFile> module
-by Luis E. Munoz.
+Anton Berezin created the versions up until 0.5.  Wes Hardaker at
+Sparta implemented the DNSSEC patches and took over maintaince of the
+module from 0.6 onward.
 
-Many thanks to Phil Regnauld and Luis E. Munoz for discussions.
+Anton's original CREDITS section:
+
+  This module was largely inspired by the I<Net::DNS::ZoneFile> module
+  by Luis E. Munoz.
+
+  Many thanks to Phil Regnauld and Luis E. Munoz for discussions.
 
 =head1 SEE ALSO
 
-Net::DNS(3), Net::DNS::RR(3).
+http://www.dnssec-tools.org/, Net::DNS(3), Net::DNS::RR(3), Net::DNS::SEC(3)
 
 =cut
