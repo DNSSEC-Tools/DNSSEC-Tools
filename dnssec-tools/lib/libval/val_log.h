ec1a1536dcf06902a911d158b88ae47286a234d1
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2005-11-15 19:14:58 +0000

    Include prototypes of all log related functions.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1132 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libval/val_log.h
+++ dnssec-tools/lib/libval/val_log.h
@@ -1,15 +1,22 @@
+
 /*
  * Copyright 2005 SPARTA, Inc.  All rights reserved.
  * See the COPYING file distributed with this software for details.
- *
- * This is a header file for the validator logger.
- */
-
+ */ 
 #ifndef VAL_LOG_H
 #define VAL_LOG_H
-#include <stdio.h>
-#include <stdarg.h>
-extern const int log_level;
-/* void val_log (FILE *fp, int level, const char *template, ...) */
-void val_log (const char *template, ...);
+
+#include <syslog.h>
+#include "val_parse.h"
+#include "val_log.h"
+
+void val_log_rrset(val_context_t *ctx, int level, struct rrset_rec *rrset);
+void val_log_base64(val_context_t *ctx, int level, unsigned char * message, int message_len);
+void val_log_rrsig_rdata (val_context_t *ctx, int level, const char *prefix, val_rrsig_rdata_t *rdata);
+void val_log_dnskey_rdata (val_context_t *ctx, int level, const char *prefix, val_dnskey_rdata_t *rdata);
+void val_log_assertion_chain(val_context_t *ctx, int level, u_char *name_n, u_int16_t class_h, u_int16_t type_h, 
+				struct query_chain *queries, struct val_result *results);
+void val_log (val_context_t *ctx, int level, const char *template, ...);
+char *p_val_error(int valerrno);
+
 #endif
