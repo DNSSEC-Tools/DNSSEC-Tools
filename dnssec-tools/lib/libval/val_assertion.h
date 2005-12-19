02f9777520b6f55a94cc4625b6d9408a3517ef17
Suresh Krishnaswamy <hserus>
2005-12-19 20:29:03 +0000

    Allow other files to access the add_to_query_chain() function.

---
+++ dnssec-tools/lib/libval/val_assertion.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2005 SPARTA, Inc.  All rights reserved.
+ * See the COPYING file distributed with this software for details.
+ */
+#ifndef VAL_ASSERTION_H
+#define VAL_ASSERTION_H
+
+int add_to_query_chain(struct query_chain **queries, u_char *name_n,
+                       const u_int16_t type_h, const u_int16_t class_h);
+
+#endif
+
