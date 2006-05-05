7c61e1ba9ffdea61dcf2c0a11eba14931f724096
rstory <rstory@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-05-05 22:19:21 +0000

    add dnssec-tools-config.h
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1550 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/lib/libsres/ns_samedomain.c
+++ dnssec-tools/lib/libsres/ns_samedomain.c
@@ -14,6 +14,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
  * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include "../../dnssec-tools-config.h"
 
 #include <sys/types.h>
 #include <errno.h>
