99db7f06a6218a9359b3338905a572a6a115d258
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-08-03 20:56:03 +0000

    - Maintain separate cache for referral information - Add routine get_matching_nslist() to retrieve matching nslist for a given query.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@1835 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_cache.h
+++ dnssec-tools/validator/libval/val_cache.h
@@ -15,5 +15,8 @@ int get_cached_rrset(u_int8_t *name_n, u_int16_t class_h,
 		u_int16_t type_h, struct rrset_rec **cloned_answer);
 int free_validator_cache();
 int get_root_ns(struct name_server **ns);
+int get_matching_nslist(struct val_query_chain  *matched_q, 
+					struct val_query_chain **queries,
+					struct name_server **ref_ns_list);
 
 #endif
