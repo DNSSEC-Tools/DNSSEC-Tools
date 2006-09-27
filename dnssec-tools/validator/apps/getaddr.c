dc8f3a59b0d425a0df8449cc2106b31235e2c6fc
rstory <rstory@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-09-27 19:06:21 +0000

    run indent
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2054 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/getaddr.c
+++ dnssec-tools/validator/apps/getaddr.c
@@ -39,192 +39,203 @@ static struct option prog_options[] = {
 };
 #endif
 
-void usage(char *progname)
+void
+usage(char *progname)
 {
-	fprintf(stderr, "Usage: %s [options] <hostname|IPv4 address|IPv6 address>\n", progname);
-	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "\t-h, --help                      display usage and exit\n");
-	fprintf(stderr, "\t-n, --novalidate                do not use the validator\n");
-	fprintf(stderr, "\t-c, --canonname                 use the AI_CANONNAME flag\n");
-	fprintf(stderr, "\t-s, --service=<PORT|SERVICE>    transport-layer port or service name\n");
+    fprintf(stderr,
+            "Usage: %s [options] <hostname|IPv4 address|IPv6 address>\n",
+            progname);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr,
+            "\t-h, --help                      display usage and exit\n");
+    fprintf(stderr,
+            "\t-n, --novalidate                do not use the validator\n");
+    fprintf(stderr,
+            "\t-c, --canonname                 use the AI_CANONNAME flag\n");
+    fprintf(stderr,
+            "\t-s, --service=<PORT|SERVICE>    transport-layer port or service name\n");
 }
 
-static int validate = 0;
+static int      validate = 0;
 
 #define ADDRINFO_TYPE     0
 #define VAL_ADDRINFO_TYPE 1
 
-static void print_addrinfo(int type, void* ainfo)
+static void
+print_addrinfo(int type, void *ainfo)
 {
-	struct sockaddr_in  *s_inaddr = NULL;
-	struct sockaddr_in6 *s_in6addr = NULL;
-	struct addrinfo *a = (struct addrinfo *)ainfo;
-	char buf[INET6_ADDRSTRLEN];
-	
-	while (a != NULL) {
-		printf("{\n");
-		printf("\tFlags:     %d [", a->ai_flags);
-		if (a->ai_flags & AI_PASSIVE) printf("AI_PASSIVE ");
-		if (a->ai_flags & AI_CANONNAME) printf("AI_CANONNAME ");
-		if (a->ai_flags & AI_NUMERICHOST) printf("AI_NUMERICHOST ");
-		if (a->ai_flags & AI_V4MAPPED) printf("AI_V4MAPPED ");
-		if (a->ai_flags & AI_ALL) printf("AI_ALL ");
-		if (a->ai_flags & AI_ADDRCONFIG) printf("AI_ADDRCONFIG ");
-		//		if (a->ai_flags & AI_NUMERICSERV) printf("AI_NUMERICSERV ");
-		printf("]\n");
-		printf("\tFamily:    %d [%s]\n", a->ai_family,
-		       (a->ai_family == AF_UNSPEC)? "AF_UNSPEC":
-		       (a->ai_family == AF_INET)? "AF_INET":
-		       (a->ai_family == AF_INET6)? "AF_INET6":
-		       "Unknown");
-		printf("\tSockType:  %d [%s]\n", a->ai_socktype,
-		       (a->ai_socktype == SOCK_STREAM)? "SOCK_STREAM":
-		       (a->ai_socktype == SOCK_DGRAM)? "SOCK_DGRAM":
-		       (a->ai_socktype == SOCK_RAW)? "SOCK_RAW":
-		       "Unknown");
-		printf("\tProtocol:  %d [%s]\n", a->ai_protocol,
-		       (a->ai_protocol == IPPROTO_IP)? "IPPROTO_IP":
-		       (a->ai_protocol == IPPROTO_TCP)? "IPPROTO_TCP":
-		       (a->ai_protocol == IPPROTO_UDP)? "IPPROTO_UDP":
-		       "Unknown");
-		printf("\tAddrLen:   %d\n", a->ai_addrlen);
-		
-		if (a->ai_addr != NULL) {
-			printf("\tAddrPtr:   %p\n", a->ai_addr);
-			if (a->ai_family == AF_INET) {
-				s_inaddr = (struct sockaddr_in *) (a->ai_addr);
-				printf("\tAddr:      %s\n",
-				       inet_ntop(AF_INET,
-						 &(s_inaddr->sin_addr),
-						 buf, INET6_ADDRSTRLEN));
-			}
-			else if (a->ai_family == AF_INET6) {
-				s_in6addr= (struct sockaddr_in6 *)(a->ai_addr);
-				printf("\tAddr:      %s\n",
-				       inet_ntop(AF_INET6,
-						 &(s_in6addr->sin6_addr),
-						 buf, INET6_ADDRSTRLEN));
-			}
-			else
-				printf("\tAddr:      Cannot parse address. Unknown protocol family\n");
-		}
-		else
-			printf("\tAddr:      (null)\n");
-		
-		if (a->ai_canonname)
-			printf("\tCanonName: %s\n", a->ai_canonname);
-		else
-			printf("\tCanonName: (null)\n");
-		
-		if (type == VAL_ADDRINFO_TYPE) {
-			printf("\tValStatus: %s\n", p_val_error(((struct val_addrinfo*)a)->ai_val_status));
-		}
-		printf("}\n");
-		
-		a = (struct addrinfo*) (a->ai_next);
-	}
+    struct sockaddr_in *s_inaddr = NULL;
+    struct sockaddr_in6 *s_in6addr = NULL;
+    struct addrinfo *a = (struct addrinfo *) ainfo;
+    char            buf[INET6_ADDRSTRLEN];
+
+    while (a != NULL) {
+        printf("{\n");
+        printf("\tFlags:     %d [", a->ai_flags);
+        if (a->ai_flags & AI_PASSIVE)
+            printf("AI_PASSIVE ");
+        if (a->ai_flags & AI_CANONNAME)
+            printf("AI_CANONNAME ");
+        if (a->ai_flags & AI_NUMERICHOST)
+            printf("AI_NUMERICHOST ");
+        if (a->ai_flags & AI_V4MAPPED)
+            printf("AI_V4MAPPED ");
+        if (a->ai_flags & AI_ALL)
+            printf("AI_ALL ");
+        if (a->ai_flags & AI_ADDRCONFIG)
+            printf("AI_ADDRCONFIG ");
+        //              if (a->ai_flags & AI_NUMERICSERV) printf("AI_NUMERICSERV ");
+        printf("]\n");
+        printf("\tFamily:    %d [%s]\n", a->ai_family,
+               (a->ai_family == AF_UNSPEC) ? "AF_UNSPEC" :
+               (a->ai_family == AF_INET) ? "AF_INET" :
+               (a->ai_family == AF_INET6) ? "AF_INET6" : "Unknown");
+        printf("\tSockType:  %d [%s]\n", a->ai_socktype,
+               (a->ai_socktype == SOCK_STREAM) ? "SOCK_STREAM" :
+               (a->ai_socktype == SOCK_DGRAM) ? "SOCK_DGRAM" :
+               (a->ai_socktype == SOCK_RAW) ? "SOCK_RAW" : "Unknown");
+        printf("\tProtocol:  %d [%s]\n", a->ai_protocol,
+               (a->ai_protocol == IPPROTO_IP) ? "IPPROTO_IP" :
+               (a->ai_protocol == IPPROTO_TCP) ? "IPPROTO_TCP" :
+               (a->ai_protocol == IPPROTO_UDP) ? "IPPROTO_UDP" :
+               "Unknown");
+        printf("\tAddrLen:   %d\n", a->ai_addrlen);
+
+        if (a->ai_addr != NULL) {
+            printf("\tAddrPtr:   %p\n", a->ai_addr);
+            if (a->ai_family == AF_INET) {
+                s_inaddr = (struct sockaddr_in *) (a->ai_addr);
+                printf("\tAddr:      %s\n",
+                       inet_ntop(AF_INET,
+                                 &(s_inaddr->sin_addr),
+                                 buf, INET6_ADDRSTRLEN));
+            } else if (a->ai_family == AF_INET6) {
+                s_in6addr = (struct sockaddr_in6 *) (a->ai_addr);
+                printf("\tAddr:      %s\n",
+                       inet_ntop(AF_INET6,
+                                 &(s_in6addr->sin6_addr),
+                                 buf, INET6_ADDRSTRLEN));
+            } else
+                printf
+                    ("\tAddr:      Cannot parse address. Unknown protocol family\n");
+        } else
+            printf("\tAddr:      (null)\n");
+
+        if (a->ai_canonname)
+            printf("\tCanonName: %s\n", a->ai_canonname);
+        else
+            printf("\tCanonName: (null)\n");
+
+        if (type == VAL_ADDRINFO_TYPE) {
+            printf("\tValStatus: %s\n",
+                   p_val_error(((struct val_addrinfo *) a)->
+                               ai_val_status));
+        }
+        printf("}\n");
+
+        a = (struct addrinfo *) (a->ai_next);
+    }
 }
 
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-	char *node = NULL;
-	char *service = NULL;
-	struct addrinfo hints;
-	struct val_addrinfo *val_ainfo = NULL;
-	struct addrinfo *ainfo = NULL;
-	int retval;
-	int getcanonname = 0;
-	int portspecified = 0;
-        val_log_t *logp;
+    char           *node = NULL;
+    char           *service = NULL;
+    struct addrinfo hints;
+    struct val_addrinfo *val_ainfo = NULL;
+    struct addrinfo *ainfo = NULL;
+    int             retval;
+    int             getcanonname = 0;
+    int             portspecified = 0;
+    val_log_t      *logp;
 
-	// Parse the command line
-	validate = 1;
-	while (1) {
-		int c;
+    // Parse the command line
+    validate = 1;
+    while (1) {
+        int             c;
 #ifdef HAVE_GETOPT_LONG
-		int opt_index = 0;
+        int             opt_index = 0;
 #ifdef HAVE_GETOPT_LONG_ONLY
-		c = getopt_long_only (argc, argv, "hcno:s:",
-					  prog_options, &opt_index);
+        c = getopt_long_only(argc, argv, "hcno:s:",
+                             prog_options, &opt_index);
 #else
-		c = getopt_long (argc, argv, "hcno:s:",
-					      prog_options, &opt_index);
+        c = getopt_long(argc, argv, "hcno:s:", prog_options, &opt_index);
 #endif
-#else /* only have getopt */
-		c = getopt (argc, argv, "hcno:s:");
+#else                           /* only have getopt */
+        c = getopt(argc, argv, "hcno:s:");
 #endif
 
-		if (c == -1) {
-			break;
-		}
+        if (c == -1) {
+            break;
+        }
 
-		switch (c) {
-		case 'h':
-			usage(argv[0]);
-			return 0;
-		case 'n':
-			validate = 0;
-			break;
-                case 'o':
-                    logp = val_log_add_optarg(optarg, 1);
-                    if (NULL == logp) { /* err msg already logged */
-                        usage(argv[0]);
-                        return 1;
-                    }
-                    break;
+        switch (c) {
+        case 'h':
+            usage(argv[0]);
+            return 0;
+        case 'n':
+            validate = 0;
+            break;
+        case 'o':
+            logp = val_log_add_optarg(optarg, 1);
+            if (NULL == logp) { /* err msg already logged */
+                usage(argv[0]);
+                return 1;
+            }
+            break;
 
-		case 's':
-		        portspecified = 1;
-			service = optarg;
-			break;
-		case 'c':
-			getcanonname = 1;
-			break;
-		default:
-			fprintf(stderr, "Invalid option %s\n", argv[optind - 1]);
-			usage(argv[0]);
-			return 1;
-		}
-	}
+        case 's':
+            portspecified = 1;
+            service = optarg;
+            break;
+        case 'c':
+            getcanonname = 1;
+            break;
+        default:
+            fprintf(stderr, "Invalid option %s\n", argv[optind - 1]);
+            usage(argv[0]);
+            return 1;
+        }
+    }
 
-	if (optind < argc) {
-		node = argv[optind++];
-	}
-	else {
-		fprintf(stderr, "Error: node name not specified\n");
-		usage(argv[0]);
-		return 1;
-	}
+    if (optind < argc) {
+        node = argv[optind++];
+    } else {
+        fprintf(stderr, "Error: node name not specified\n");
+        usage(argv[0]);
+        return 1;
+    }
 
-	bzero(&hints, sizeof(struct addrinfo));
-	if (getcanonname) {
-		hints.ai_flags |= AI_CANONNAME;
-	}
+    bzero(&hints, sizeof(struct addrinfo));
+    if (getcanonname) {
+        hints.ai_flags |= AI_CANONNAME;
+    }
 
-	if (validate) {
-		retval = val_getaddrinfo(NULL, node, service, &hints, &val_ainfo);
+    if (validate) {
+        retval = val_getaddrinfo(NULL, node, service, &hints, &val_ainfo);
 
-		if (retval != 0) {
-			printf("Error in val_getaddrinfo(): %s\n", gai_strerror(retval));
-			exit(1);
-		}
-		else {
-			print_addrinfo(VAL_ADDRINFO_TYPE, val_ainfo);
-		}
-		
-		/* cleanup */
-		free_val_addrinfo(val_ainfo);
-	}
-	else {
-		retval = getaddrinfo(node, service, &hints, &ainfo);
-		if (retval != 0) {
-			printf("Error in getaddrinfo(): %s\n", gai_strerror(retval));
-			exit(1);
-		}
-		else {
-			print_addrinfo(ADDRINFO_TYPE, ainfo);
-		}
-	}
-	return 0;
+        if (retval != 0) {
+            printf("Error in val_getaddrinfo(): %s\n",
+                   gai_strerror(retval));
+            exit(1);
+        } else {
+            print_addrinfo(VAL_ADDRINFO_TYPE, val_ainfo);
+        }
+
+        /*
+         * cleanup 
+         */
+        free_val_addrinfo(val_ainfo);
+    } else {
+        retval = getaddrinfo(node, service, &hints, &ainfo);
+        if (retval != 0) {
+            printf("Error in getaddrinfo(): %s\n", gai_strerror(retval));
+            exit(1);
+        } else {
+            print_addrinfo(ADDRINFO_TYPE, ainfo);
+        }
+    }
+    return 0;
 }
--- dnssec-tools/validator/apps/gethost.c
+++ dnssec-tools/validator/apps/gethost.c
@@ -34,188 +34,228 @@ static struct option prog_options[] = {
 
 #define AUX_BUFLEN 16000
 
-void usage(char *progname)
+void
+usage(char *progname)
 {
-	fprintf(stderr, "Usage: %s [options] name\n", progname);
-	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "\t-h, --help                      display usage and exit\n");
-	fprintf(stderr, "\t-n, --novalidate                do not use the validator\n");
-	fprintf(stderr, "\t-r, --reentrant                 use reentrant versions of functions\n");
-	fprintf(stderr, "\t-f, --family=[AF_INET|AF_INET6] address family\n");
-	fprintf(stderr, "\t                                AF_INET for IPv4 addresses,\n");
-	fprintf(stderr, "\t                                and AF_INET6 for IPv6 addresses\n");
+    fprintf(stderr, "Usage: %s [options] name\n", progname);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr,
+            "\t-h, --help                      display usage and exit\n");
+    fprintf(stderr,
+            "\t-n, --novalidate                do not use the validator\n");
+    fprintf(stderr,
+            "\t-r, --reentrant                 use reentrant versions of functions\n");
+    fprintf(stderr, "\t-f, --family=[AF_INET|AF_INET6] address family\n");
+    fprintf(stderr,
+            "\t                                AF_INET for IPv4 addresses,\n");
+    fprintf(stderr,
+            "\t                                and AF_INET6 for IPv6 addresses\n");
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-	struct hostent hentry;
-	char auxbuf[AUX_BUFLEN];
-	struct hostent *result = NULL;
-	
-	int i;
-	val_status_t val_status;
-	int herrno = 0;
-	int dovalidate = 1;
-	int familyspecified = 0;
-	int usereentrant = 0;
-	char *name;
-	int af = AF_INET;
-	char buf[INET6_ADDRSTRLEN];
-	int retval = 0;
+    struct hostent  hentry;
+    char            auxbuf[AUX_BUFLEN];
+    struct hostent *result = NULL;
 
-	bzero (&hentry, sizeof(struct hostent));
-	bzero (auxbuf, AUX_BUFLEN);
-	
-	// Parse the command line
+    int             i;
+    val_status_t    val_status;
+    int             herrno = 0;
+    int             dovalidate = 1;
+    int             familyspecified = 0;
+    int             usereentrant = 0;
+    char           *name;
+    int             af = AF_INET;
+    char            buf[INET6_ADDRSTRLEN];
+    int             retval = 0;
 
-	while (1) {
-		int c;
+    bzero(&hentry, sizeof(struct hostent));
+    bzero(auxbuf, AUX_BUFLEN);
+
+    // Parse the command line
+
+    while (1) {
+        int             c;
 #ifdef HAVE_GETOPT_LONG
-		int opt_index = 0;
+        int             opt_index = 0;
 #ifdef HAVE_GETOPT_LONG_ONLY
-		c = getopt_long_only (argc, argv, "hrnf:",
-					  prog_options, &opt_index);
+        c = getopt_long_only(argc, argv, "hrnf:",
+                             prog_options, &opt_index);
 #else
-		c = getopt_long (argc, argv, "hrnf:",
-					      prog_options, &opt_index);
+        c = getopt_long(argc, argv, "hrnf:", prog_options, &opt_index);
 #endif
-#else /* only have getopt */
-		c = getopt (argc, argv, "hrnf:");
+#else                           /* only have getopt */
+        c = getopt(argc, argv, "hrnf:");
 #endif
 
-		if (c == -1) {
-			break;
-		}
-
-		switch (c) {
-		case 'h':
-			usage(argv[0]);
-			return 0;
-		case 'n':
-			dovalidate = 0;
-			break;
-		case 'f':
-		        familyspecified = 1;
-			if (strcasecmp(optarg, "AF_INET") == 0) {
-				af = AF_INET;
-			}
-			else if (strcasecmp(optarg, "AF_INET6") == 0) {
-				af = AF_INET6;
-			}
-			else {
-				fprintf(stderr, "Invalid family %s\n", optarg);
-				usage(argv[0]);
-				return 1;
-			}
-			break;
-		case 'r':
-			usereentrant = 1;
-			break;
-		default:
-			fprintf(stderr, "Invalid option %s\n", argv[optind - 1]);
-			usage(argv[0]);
-			return 1;
-		}
-	}
-
-
-	if (optind < argc) {
-		name = argv[optind++];
-	}
-	else {
-		fprintf(stderr, "Error: name not specified\n");
-		usage(argv[0]);
-		return 1;
-	}
-
-	if (dovalidate) {
-		if (usereentrant) {
-#ifdef HAVE_GETHOSTBYNAME2
-			if (familyspecified)
-				retval = val_gethostbyname2_r(NULL, name, af, &hentry, auxbuf, AUX_BUFLEN,
-							      &result, &herrno, &val_status);
-			else
-#endif
-				retval = val_gethostbyname_r(NULL, name, &hentry, auxbuf, AUX_BUFLEN,
-							     &result, &herrno, &val_status);
-		}
-		else {
-			if (familyspecified)
-				result = val_gethostbyname2 (NULL, name, af, &val_status);
-			else
-				result = val_gethostbyname (NULL, name, &val_status);
-		}
-	}
-	else {
-		if (usereentrant) {
-#if 0
-			if (familyspecified)
-				retval = gethostbyname2_r(name, af, &hentry, auxbuf, AUX_BUFLEN,
-							  &result, &herrno);
-			else
-				retval = gethostbyname_r(name, &hentry, auxbuf, AUX_BUFLEN,
-							 &result, &herrno);
-#endif
-		}
-		else {
-#ifdef HAVE_GETHOSTBYNAME2
-			if (familyspecified)
-				result = gethostbyname2 (name, af);
-			else
-#endif
-				result = gethostbyname (name);
-		}
-	}
-
-	if (result != NULL) {
-		printf("\n\th_name = %s\n", result->h_name);
-		if (result->h_aliases) {
-			printf("\th_aliases = \n");
-			for (i=0; result->h_aliases[i] != 0; i++) {
-				printf("\t\t[%d] = %s\n", i, result->h_aliases[i]);
-			} 
-		}
-		else
-			printf ("\th_aliases = NULL\n");
-		if (result->h_addrtype == AF_INET) {
-			printf("\th_addrtype = AF_INET\n");
-		}
-		else if (result->h_addrtype == AF_INET6) {
-			printf("\th_addrtype = AF_INET6\n");
-		}
-		else {
-			printf("\th_addrtype = %d\n", result->h_addrtype);
-		}
-		printf("\th_length = %d\n", result->h_length);
-		printf("\th_addr_list = \n");
-		for (i=0; result->h_addr_list[i] != 0; i++) {
-			bzero(buf, INET6_ADDRSTRLEN);
-			printf("\t\t[%d] = %s\n", i,
-			       inet_ntop(result->h_addrtype,
-					 result->h_addr_list[i],
-					 buf, INET6_ADDRSTRLEN));
-		}
-	}
-	else {
-	   	printf("result is NULL\n"); 
+        if (c == -1) {
+            break;
         }
-	if (dovalidate) {
-		printf("Validation status = %s\n", p_val_error(val_status));
-	}
-	if (usereentrant) {
-#ifdef HAVE_HSTRERROR
-		printf("h_errno = %s\n", hstrerror(herrno));
-#else
-		printf("h_errno = %d\n", herrno);
-#endif
-	}
-	else {
-#ifdef HAVE_HSTRERROR
-		printf("h_errno = %s\n", hstrerror(h_errno));
-#else
-		printf("h_errno = %d\n", h_errno);
-#endif
-	}
 
-	return 0;
+        switch (c) {
+        case 'h':
+            usage(argv[0]);
+            return 0;
+        case 'n':
+            dovalidate = 0;
+            break;
+        case 'f':
+            familyspecified = 1;
+            if (strcasecmp(optarg, "AF_INET") == 0) {
+                af = AF_INET;
+            } else if (strcasecmp(optarg, "AF_INET6") == 0) {
+                af = AF_INET6;
+            } else {
+                fprintf(stderr, "Invalid family %s\n", optarg);
+                usage(argv[0]);
+                return 1;
+            }
+            break;
+        case 'r':
+            usereentrant = 1;
+            break;
+        default:
+            fprintf(stderr, "Invalid option %s\n", argv[optind - 1]);
+            usage(argv[0]);
+            return 1;
+        }
+    }
+
+
+    if (optind < argc) {
+        name = argv[optind++];
+    } else {
+        fprintf(stderr, "Error: name not specified\n");
+        usage(argv[0]);
+        return 1;
+    }
+
+    <<<<<<<.mine if (dovalidate) {
+        if (usereentrant) {
+            if (familyspecified)
+                retval =
+                    val_gethostbyname2_r(NULL, name, af, &hentry, auxbuf,
+                                         AUX_BUFLEN, &result, &herrno,
+                                         &val_status);
+            else
+                retval =
+                    val_gethostbyname_r(NULL, name, &hentry, auxbuf,
+                                        AUX_BUFLEN, &result, &herrno,
+                                        &val_status);
+        } else {
+            if (familyspecified)
+                result = val_gethostbyname2(NULL, name, af, &val_status);
+            else
+                result = val_gethostbyname(NULL, name, &val_status);
+        }
+    } else {
+        if (usereentrant) {
+            == == == = if (dovalidate) {
+                if (usereentrant) {
+#ifdef HAVE_GETHOSTBYNAME2
+                    if (familyspecified)
+                        retval =
+                            val_gethostbyname2_r(NULL, name, af, &hentry,
+                                                 auxbuf, AUX_BUFLEN,
+                                                 &result, &herrno,
+                                                 &val_status);
+                    else
+#endif
+                        retval =
+                            val_gethostbyname_r(NULL, name, &hentry,
+                                                auxbuf, AUX_BUFLEN,
+                                                &result, &herrno,
+                                                &val_status);
+                } else {
+                    if (familyspecified)
+                        result =
+                            val_gethostbyname2(NULL, name, af,
+                                               &val_status);
+                    else
+                        result =
+                            val_gethostbyname(NULL, name, &val_status);
+                }
+            } else {
+                if (usereentrant) {
+                    >>>>>>>.r2053
+#if 0
+                        if (familyspecified)
+                        retval =
+                            gethostbyname2_r(name, af, &hentry, auxbuf,
+                                             AUX_BUFLEN, &result, &herrno);
+                    else
+                        retval =
+                            gethostbyname_r(name, &hentry, auxbuf,
+                                            AUX_BUFLEN, &result, &herrno);
+#endif
+                <<<<<<<.mine} else {
+                    if (familyspecified)
+                        result = gethostbyname2(name, af);
+                    else
+                        result = gethostbyname(name);
+                }
+            }
+        == == == =} else {
+#ifdef HAVE_GETHOSTBYNAME2
+            if (familyspecified)
+                result = gethostbyname2(name, af);
+            else
+#endif
+                result = gethostbyname(name);
+        }
+    }
+    >>>>>>>.r2053 if (result != NULL) {
+        printf("\n\th_name = %s\n", result->h_name);
+        if (result->h_aliases) {
+            printf("\th_aliases = \n");
+            for (i = 0; result->h_aliases[i] != 0; i++) {
+                printf("\t\t[%d] = %s\n", i, result->h_aliases[i]);
+            }
+        } else
+            printf("\th_aliases = NULL\n");
+        if (result->h_addrtype == AF_INET) {
+            printf("\th_addrtype = AF_INET\n");
+        } else if (result->h_addrtype == AF_INET6) {
+            printf("\th_addrtype = AF_INET6\n");
+        } else {
+            printf("\th_addrtype = %d\n", result->h_addrtype);
+        }
+        <<<<<<<.mine printf("\th_length = %d\n", result->h_length);
+        printf("\th_addr_list = \n");
+        for (i = 0; result->h_addr_list[i] != 0; i++) {
+            bzero(buf, INET6_ADDRSTRLEN);
+            printf("\t\t[%d] = %s\n", i,
+                   inet_ntop(result->h_addrtype,
+                             result->h_addr_list[i],
+                             buf, INET6_ADDRSTRLEN));
+        }
+    } else {
+        printf("result is NULL\n");
+    }
+    if (dovalidate) {
+        printf("Validation status = %s\n", p_val_error(val_status));
+    }
+    if (usereentrant) {
+        printf("h_errno = %s\n", hstrerror(herrno));
+    } else {
+        printf("h_errno = %s\n", hstrerror(h_errno));
+    }
+    == == == = if (dovalidate) {
+        printf("Validation status = %s\n", p_val_error(val_status));
+    }
+    if (usereentrant) {
+#ifdef HAVE_HSTRERROR
+        printf("h_errno = %s\n", hstrerror(herrno));
+#else
+        printf("h_errno = %d\n", herrno);
+#endif
+    } else {
+#ifdef HAVE_HSTRERROR
+        printf("h_errno = %s\n", hstrerror(h_errno));
+#else
+        printf("h_errno = %d\n", h_errno);
+#endif
+    }
+    >>>>>>>.r2053 return 0;
 }
