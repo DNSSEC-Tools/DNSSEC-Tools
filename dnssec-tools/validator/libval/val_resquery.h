93c612fbd3fd0e1ac8f6a33d1b662537d68754f5
rstory <rstory@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-09-27 18:00:17 +0000

    - SAVE_RR_TO_LIST   - move from header to above function where used   - add appropriate cleanup on return cases - digest_response()   - free learned_* rrsets before returning   - add 'else' to save unnecessary comparison - val_resquery_send(): rename var to *_p to indicate ascii domain - val_resquery_rcv()   - rename var to *_p to indicate ascii domain   - free response_data if not used   - free response domain info when done w/it - find_next_zonecut(): add context param so no temporary needed
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2048 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_resquery.h
+++ dnssec-tools/validator/libval/val_resquery.h
@@ -27,30 +27,9 @@ int             val_resquery_rcv(val_context_t * context,
                                  struct domain_info **response,
                                  struct val_query_chain **queries);
 
-int             find_next_zonecut(struct rrset_rec *rrset,
+int             find_next_zonecut(val_context_t * context,
+                                  struct rrset_rec *rrset,
                                   u_int8_t * curzone_n,
                                   u_int8_t ** name_n);
-
-#define SAVE_RR_TO_LIST(respondent_server, listtype, name_n, type_h, set_type_h,\
-				class_h, ttl_h, hptr, rdata, rdata_len_h, from_section, authoritive, zonecut_n) \
-	do { \
-            struct rrset_rec *rr_set;\
-            int ret_val;\
-            rr_set = find_rr_set (respondent_server, listtype, name_n, type_h, set_type_h,\
-                             class_h, ttl_h, hptr, rdata, from_section,authoritive, zonecut_n);\
-            if (rr_set==NULL) return VAL_OUT_OF_MEMORY;\
-            if (type_h != ns_t_rrsig)\
-            {\
-                /* Add this record to its chain of rr_rec's. */\
-                if ((ret_val = add_to_set(rr_set,rdata_len_h,rdata))!=VAL_NO_ERROR) \
-                    return ret_val;\
-            }\
-            else\
-            {\
-                /* Add this record to the sig of rrset_rec. */\
-                if ((ret_val = add_as_sig(rr_set,rdata_len_h,rdata))!=VAL_NO_ERROR)\
-                    return ret_val;\
-            }\
-	} while (0)
 
 #endif                          /* VAL_RESQUERY_H */
