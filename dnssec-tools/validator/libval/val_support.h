1c23ad61a72f5966dad23e5b136200a592d172f0
rstory <rstory@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-10-06 17:01:28 +0000

    new copy_rr_rec_list() function
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2104 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_support.h
+++ dnssec-tools/validator/libval/val_support.h
@@ -106,6 +106,8 @@ void            lower_name(u_int8_t rdata[], size_t * index);
 void            lower(u_int16_t type_h, u_int8_t * rdata, int len);
 struct rr_rec  *copy_rr_rec(u_int16_t type_h, struct rr_rec *r,
                             int dolower);
+struct rr_rec *copy_rr_rec_list(u_int16_t type_h, struct rr_rec *o_rr,
+                                int dolower);
 int             link_rr(struct rr_rec **cs, struct rr_rec *cr);
 struct rrset_rec *copy_rrset_rec(struct rrset_rec *rr_set);
 
