5cde260d76336129070c59d743e04ce457830fef
Suresh Krishnaswamy <hserus>
2006-10-16 13:53:42 +0000

    Move VAL_A_RRSIG_VERIFIED close to all the "success" conditions Add a new VAL_A_WCARD_VERIFIED state for assertions that were wildcard expanded.

--- dnssec-tools/validator/libval/val_errors.h
+++ dnssec-tools/validator/libval/val_errors.h
@@ -62,13 +62,13 @@
 /*
  * error and dont want to check if provably unsecure either 
  */
-#define VAL_A_BAD_BASE VAL_A_LAST_ERROR
+#define VAL_A_BAD_BASE VAL_A_LAST_ERROR /* 15 */
 #define VAL_A_IRRELEVANT_PROOF (VAL_A_BAD_BASE+1)
 #define VAL_A_DNSSEC_VERSION_ERROR (VAL_A_BAD_BASE+2)
 #define VAL_A_TOO_MANY_LINKS (VAL_A_BAD_BASE+3)
 #define VAL_A_UNKNOWN_DNSKEY_PROTO (VAL_A_BAD_BASE+4)
 #define VAL_A_FLOOD_ATTACK_DETECTED	(VAL_A_BAD_BASE+5)
-#define VAL_A_DNS_ERROR_BASE (VAL_A_BAD_BASE+10)
+#define VAL_A_DNS_ERROR_BASE (VAL_A_BAD_BASE+10) /* 25 */
 /*
  * DNS errors lie within this range, 
  * there are SR_LAST_ERROR (22) of them in total
@@ -78,12 +78,12 @@
 #define SR_CONFLICTING_ANSWERS (SR_LAST_ERROR+3)
 #define VAL_A_DNS_ERROR_LAST (VAL_A_DNS_ERROR_BASE + SR_CONFLICTING_ANSWERS)
 
-#define VAL_A_LAST_BAD VAL_A_DNS_ERROR_LAST     /* VAL_A_ERROR_BASE+40 */
+#define VAL_A_LAST_BAD VAL_A_DNS_ERROR_LAST     /* 50 */
 
 /*
  * "Error, but can prove the chain-of-trust above this" states 
  */
-#define VAL_A_FAIL_BASE VAL_A_LAST_BAD  /* VAL_A_ERROR_BASE+40 */
+#define VAL_A_FAIL_BASE VAL_A_LAST_BAD  /* 50 */
 #define VAL_A_DNSKEY_NOMATCH (VAL_A_FAIL_BASE+1)        /*RRSIG was created by a DNSKEY that does not exist in the apex keyset. */
 #define VAL_A_WRONG_LABEL_COUNT (VAL_A_FAIL_BASE+2)     /*The number of labels on the signature is greater than the the count given in the RRSIG RDATA. */
 #define VAL_A_SECURITY_LAME (VAL_A_FAIL_BASE+3) /*RRSIG created by a key that does not exist in the parent DS record set. */
@@ -92,33 +92,35 @@
 #define VAL_A_RRSIG_EXPIRED	(VAL_A_FAIL_BASE+6)     /*The RRSIG has expired. */
 #define VAL_A_ALGO_NOT_SUPPORTED (VAL_A_FAIL_BASE+7)    /* Algorithm in DNSKEY or RRSIG or DS is not supported. */
 #define VAL_A_UNKNOWN_ALGO (VAL_A_FAIL_BASE+8)  /* Unknown DNSKEY or RRSIG or DS algorithm */
-#define VAL_A_RRSIG_VERIFIED (VAL_A_FAIL_BASE+9)        /* The RRSIG verified successfully. */
-#define VAL_A_RRSIG_VERIFY_FAILED (VAL_A_FAIL_BASE+10)  /*The RRSIG did not verify. */
-#define VAL_A_NOT_VERIFIED (VAL_A_FAIL_BASE+11) /*Different RRSIGs failed for different reasons */
-#define VAL_A_KEY_TOO_LARGE (VAL_A_FAIL_BASE+12)        /*The zone is using a key size that is too large as per local policy. */
-#define VAL_A_KEY_TOO_SMALL (VAL_A_FAIL_BASE+13)        /*The zone is using a key size that is too small as per local policy */
-#define VAL_A_KEY_NOT_AUTHORIZED (VAL_A_FAIL_BASE+14)   /*The zone is using a key that is not authorized as per local policy. */
-#define VAL_A_ALGO_REFUSED (VAL_A_FAIL_BASE+15) /*Algorithm in DNSKEY or RRSIG or DS is not allowed as per local policy */
-#define VAL_A_CLOCK_SKEW (VAL_A_FAIL_BASE+16)   /*Verified but with clock skew taken into account */
-#define VAL_A_DUPLICATE_KEYTAG (VAL_A_FAIL_BASE+17)     /*Two DNSKEYs have the same keytag */
-#define VAL_A_NO_PREFERRED_SEP (VAL_A_FAIL_BASE+18)     /*There is no DNSKEY in the parent DS set that our local policy allows us to traverse */
-#define VAL_A_WRONG_RRSIG_OWNER (VAL_A_FAIL_BASE+19)    /* The RRSIG and the data that it purportedly covers have differing notions of owner name */
-#define VAL_A_RRSIG_ALGO_MISMATCH (VAL_A_FAIL_BASE+20)  /* The DNSKEY and RRSIG pair have a mismatch in their algorithm. */
-#define VAL_A_KEYTAG_MISMATCH (VAL_A_FAIL_BASE+21)      /* The DNSKEY and RRSIG pair have a mismatch in their key tags */
-#define VAL_A_LAST_FAILURE (VAL_A_FAIL_BASE+30) /* VAL_A_ERROR_BASE + 70 */
+#define VAL_A_RRSIG_VERIFY_FAILED (VAL_A_FAIL_BASE+9)  /*The RRSIG did not verify. */
+#define VAL_A_NOT_VERIFIED (VAL_A_FAIL_BASE+10) /*Different RRSIGs failed for different reasons */
+#define VAL_A_KEY_TOO_LARGE (VAL_A_FAIL_BASE+11)        /*The zone is using a key size that is too large as per local policy. */
+#define VAL_A_KEY_TOO_SMALL (VAL_A_FAIL_BASE+12)        /*The zone is using a key size that is too small as per local policy */
+#define VAL_A_KEY_NOT_AUTHORIZED (VAL_A_FAIL_BASE+13)   /*The zone is using a key that is not authorized as per local policy. */
+#define VAL_A_ALGO_REFUSED (VAL_A_FAIL_BASE+14) /*Algorithm in DNSKEY or RRSIG or DS is not allowed as per local policy */
+#define VAL_A_CLOCK_SKEW (VAL_A_FAIL_BASE+15)   /*Verified but with clock skew taken into account */
+#define VAL_A_DUPLICATE_KEYTAG (VAL_A_FAIL_BASE+16)     /*Two DNSKEYs have the same keytag */
+#define VAL_A_NO_PREFERRED_SEP (VAL_A_FAIL_BASE+17)     /*There is no DNSKEY in the parent DS set that our local policy allows us to traverse */
+#define VAL_A_WRONG_RRSIG_OWNER (VAL_A_FAIL_BASE+18)    /* The RRSIG and the data that it purportedly covers have differing notions of owner name */
+#define VAL_A_RRSIG_ALGO_MISMATCH (VAL_A_FAIL_BASE+19)  /* The DNSKEY and RRSIG pair have a mismatch in their algorithm. */
+#define VAL_A_KEYTAG_MISMATCH (VAL_A_FAIL_BASE+20)      /* The DNSKEY and RRSIG pair have a mismatch in their key tags */
+#define VAL_A_LAST_FAILURE (VAL_A_FAIL_BASE+30) /* 80 */
 
 /*
  * success or unknown result conditions 
  */
 #define VAL_A_VERIFIED (VAL_A_LAST_FAILURE+1)   /* This is a transient state, it will settle at
                                                  * VALIDATED_SUCCESS if the */
-#define VAL_A_VERIFIED_LINK (VAL_A_LAST_FAILURE+2)      /* This is a transient state, it will settle at VALIDATED_SUCCESS if the chain of trust can be completed */
-#define VAL_A_LOCAL_ANSWER (VAL_A_LAST_FAILURE+3)       /* Answer obtained locally */
-#define VAL_A_TRUST_KEY (VAL_A_LAST_FAILURE+4)  /* key is trusted */
-#define VAL_A_TRUST_ZONE (VAL_A_LAST_FAILURE+5) /* zone is trusted */
-#define VAL_A_PROVABLY_UNSECURE (VAL_A_LAST_FAILURE+6)
-#define VAL_A_BARE_RRSIG (VAL_A_LAST_FAILURE+7) /* No DNSSEC validation possible, query was for a RRSIG. */
-#define VAL_A_NO_TRUST_ANCHOR (VAL_A_LAST_FAILURE+8)    /* No trust anchor available, but components were verified */
+#define VAL_A_RRSIG_VERIFIED (VAL_A_LAST_FAILURE+2)        /* The RRSIG verified successfully. */
+#define VAL_A_WCARD_VERIFIED (VAL_A_LAST_FAILURE+3)        /* The RRSIG verified successfully after
+                                                              wildcard expansion. */
+#define VAL_A_VERIFIED_LINK (VAL_A_LAST_FAILURE+4)      /* This is a transient state, it will settle at VALIDATED_SUCCESS if the chain of trust can be completed */
+#define VAL_A_LOCAL_ANSWER (VAL_A_LAST_FAILURE+5)       /* Answer obtained locally */
+#define VAL_A_TRUST_KEY (VAL_A_LAST_FAILURE+6)  /* key is trusted */
+#define VAL_A_TRUST_ZONE (VAL_A_LAST_FAILURE+7) /* zone is trusted */
+#define VAL_A_PROVABLY_UNSECURE (VAL_A_LAST_FAILURE+8)
+#define VAL_A_BARE_RRSIG (VAL_A_LAST_FAILURE+9) /* No DNSSEC validation possible, query was for a RRSIG. */
+#define VAL_A_NO_TRUST_ANCHOR (VAL_A_LAST_FAILURE+10)    /* No trust anchor available, but components were verified */
 
 /*
  *************************************************** 
