22a2f608c93bb54d0b0f091b83ebb7199ed8e9eb
Suresh Krishnaswamy <hserus>
2006-10-16 13:53:01 +0000

    Use the new name for struct _val_authentication_chain (struct val_digested_auth_chain) Add prototype for is_trusted_zone()

--- dnssec-tools/validator/libval/val_assertion.h
+++ dnssec-tools/validator/libval/val_assertion.h
@@ -8,11 +8,12 @@
 int             add_to_query_chain(struct val_query_chain **queries,
                                    u_char * name_n, const u_int16_t type_h,
                                    const u_int16_t class_h);
-void            free_authentication_chain(struct _val_authentication_chain
+void            free_authentication_chain(struct val_digested_auth_chain
                                           *assertions);
 void            free_query_chain(struct val_query_chain *queries);
 int             val_isauthentic(val_status_t val_status);
 int             val_istrusted(val_status_t val_status);
+u_int16_t       is_trusted_zone(val_context_t * ctx, u_int8_t * name_n);
 void            val_free_result_chain(struct val_result_chain *results);
 int             val_resolve_and_check(val_context_t * context,
                                       u_char * domain_name,
