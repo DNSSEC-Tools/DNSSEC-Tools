ec90a13cb2195d09c33b6882c30fa6858559f81c
Suresh Krishnaswamy <hserus>
2006-10-16 13:50:08 +0000

    Use the new name struct val_digested_auth_chain for older struct _val_authentication_chain

--- dnssec-tools/validator/libval/val_verify.h
+++ dnssec-tools/validator/libval/val_verify.h
@@ -20,6 +20,6 @@ typedef int     val_result_t;
  * The Verifier Function.
  */
 void            verify_next_assertion(val_context_t * ctx,
-                                      struct _val_authentication_chain *as);
+                                      struct val_digested_auth_chain *as);
 
 #endif
