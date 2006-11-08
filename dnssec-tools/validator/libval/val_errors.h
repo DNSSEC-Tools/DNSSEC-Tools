0be554090c1ed483f84d5fcdac3ccfe2de5470d6
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-11-08 17:02:42 +0000

    Removed VAL_A_TOO_MANY_LINKS Removed VAL_A_FLOOD_ATTACK_DETECTED Removed VAL_A_NO_PREFERRED_SEP Renamed VAL_A_SECURITY_LAME to VAL_AC_BAD_DELEGATION Renamed VAL_A_* to VAL_AC_* Expand ALGO to ALGORITHM Expand PROTO to PROTOCOL
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2271 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_errors.h
+++ dnssec-tools/validator/libval/val_errors.h
@@ -32,14 +32,14 @@
  * Assertion initial states 
  *************************************************** 
  */
-#define VAL_A_UNSET 0
-#define VAL_A_CAN_VERIFY 1
-#define VAL_A_WAIT_FOR_TRUST 2
-#define VAL_A_WAIT_FOR_RRSIG 3
-#define VAL_A_INIT 4
-#define VAL_A_NEGATIVE_PROOF 5
-#define VAL_A_DONT_VALIDATE 6
-#define VAL_A_LAST_STATE  10    /* Closest round number above A_NEGATIVE_PROOF */
+#define VAL_AC_UNSET 0
+#define VAL_AC_CAN_VERIFY 1
+#define VAL_AC_WAIT_FOR_TRUST 2
+#define VAL_AC_WAIT_FOR_RRSIG 3
+#define VAL_AC_INIT 4
+#define VAL_AC_NEGATIVE_PROOF 5
+#define VAL_AC_DONT_VALIDATE 6
+#define VAL_AC_LAST_STATE  10    /* Closest round number above A_NEGATIVE_PROOF */
 
 /*
  *************************************************** 
@@ -50,23 +50,20 @@
 /*
  * "Cannot do anything further" states, but should check proof of non existence 
  */
-#define VAL_A_ERROR_BASE VAL_A_LAST_STATE       /* 10 */
-#define VAL_A_DATA_MISSING (VAL_A_ERROR_BASE+1)
-#define VAL_A_RRSIG_MISSING (VAL_A_ERROR_BASE+2)
-#define VAL_A_DNSKEY_MISSING (VAL_A_ERROR_BASE+3)
-#define VAL_A_DS_MISSING (VAL_A_ERROR_BASE+4)
-#define VAL_A_UNTRUSTED_ZONE (VAL_A_ERROR_BASE+5)
-#define VAL_A_LAST_ERROR VAL_A_UNTRUSTED_ZONE
+#define VAL_AC_ERROR_BASE VAL_AC_LAST_STATE       /* 10 */
+#define VAL_AC_DATA_MISSING (VAL_AC_ERROR_BASE+1)
+#define VAL_AC_RRSIG_MISSING (VAL_AC_ERROR_BASE+2)
+#define VAL_AC_DNSKEY_MISSING (VAL_AC_ERROR_BASE+3)
+#define VAL_AC_DS_MISSING (VAL_AC_ERROR_BASE+4)
+#define VAL_AC_UNTRUSTED_ZONE (VAL_AC_ERROR_BASE+5)
+#define VAL_AC_LAST_ERROR VAL_AC_UNTRUSTED_ZONE
 
 /*
  * error and dont want to check if provably unsecure either 
  */
-#define VAL_A_BAD_BASE VAL_A_LAST_ERROR /* 15 */
-#define VAL_A_DNSSEC_VERSION_ERROR (VAL_A_BAD_BASE+1)
-#define VAL_A_TOO_MANY_LINKS (VAL_A_BAD_BASE+2)
-#define VAL_A_UNKNOWN_DNSKEY_PROTO (VAL_A_BAD_BASE+3)
-#define VAL_A_FLOOD_ATTACK_DETECTED	(VAL_A_BAD_BASE+4)
-#define VAL_A_DNS_ERROR_BASE (VAL_A_BAD_BASE+10) /* 25 */
+#define VAL_AC_BAD_BASE VAL_AC_LAST_ERROR /* 15 */
+#define VAL_AC_UNKNOWN_DNSKEY_PROTOCOL (VAL_AC_BAD_BASE+1)
+#define VAL_AC_DNS_ERROR_BASE (VAL_AC_BAD_BASE+10) /* 25 */
 /*
  * DNS errors lie within this range, 
  * there are SR_LAST_ERROR (22) of them in total
@@ -74,53 +71,52 @@
 #define SR_REFERRAL_ERROR (SR_LAST_ERROR+1)     /* one more DNS error for referral failures */
 #define SR_MISSING_GLUE (SR_LAST_ERROR+2)       /* one more DNS error for referral failures */
 #define SR_CONFLICTING_ANSWERS (SR_LAST_ERROR+3)       
-#define VAL_A_DNS_ERROR_LAST (VAL_A_DNS_ERROR_BASE + SR_CONFLICTING_ANSWERS)
+#define VAL_AC_DNS_ERROR_LAST (VAL_AC_DNS_ERROR_BASE + SR_CONFLICTING_ANSWERS)
 
-#define VAL_A_LAST_BAD VAL_A_DNS_ERROR_LAST     /* 50 */
+#define VAL_AC_LAST_BAD VAL_AC_DNS_ERROR_LAST     /* 50 */
 
 /*
  * "Error, but can prove the chain-of-trust above this" states 
  */
-#define VAL_A_FAIL_BASE VAL_A_LAST_BAD  /* 50 */
-#define VAL_A_NOT_VERIFIED (VAL_A_FAIL_BASE+1) /*Different RRSIGs failed for different reasons */
+#define VAL_AC_FAIL_BASE VAL_AC_LAST_BAD  /* 50 */
+#define VAL_AC_NOT_VERIFIED (VAL_AC_FAIL_BASE+1) /*Different RRSIGs failed for different reasons */
 
 /* The signature status contains the following value */
-#define VAL_A_DNSKEY_NOMATCH (VAL_A_FAIL_BASE+2)        /*RRSIG was created by a DNSKEY that does not exist in the apex keyset. */
-#define VAL_A_WRONG_LABEL_COUNT (VAL_A_FAIL_BASE+3)     /*The number of labels on the signature is greater than the the count given in the RRSIG RDATA. */
-#define VAL_A_SECURITY_LAME (VAL_A_FAIL_BASE+4) /*RRSIG created by a key that does not exist in the parent DS record set. */
-#define VAL_A_INVALID_KEY (VAL_A_FAIL_BASE+5)        /*The key used to verify the RRSIG is not a zone key, or could not be parsed etc. */ 
-#define VAL_A_INVALID_RRSIG (VAL_A_FAIL_BASE+6)        /*The rrsig could not be parsed etc. */ 
-#define VAL_A_RRSIG_NOTYETACTIVE (VAL_A_FAIL_BASE+7)    /*The RRSIG's inception time is in the future. */
-#define VAL_A_RRSIG_EXPIRED	(VAL_A_FAIL_BASE+8)     /*The RRSIG has expired. */
-#define VAL_A_ALGO_NOT_SUPPORTED (VAL_A_FAIL_BASE+9)    /* Algorithm in DNSKEY or RRSIG or DS is not supported. */
-#define VAL_A_UNKNOWN_ALGO (VAL_A_FAIL_BASE+10)  /* Unknown DNSKEY or RRSIG or DS algorithm */
-#define VAL_A_RRSIG_VERIFY_FAILED (VAL_A_FAIL_BASE+11)  /*The RRSIG did not verify. */
-#define VAL_A_KEY_TOO_LARGE (VAL_A_FAIL_BASE+12)        /*The zone is using a key size that is too large as per local policy. */
-#define VAL_A_KEY_TOO_SMALL (VAL_A_FAIL_BASE+13)        /*The zone is using a key size that is too small as per local policy */
-#define VAL_A_KEY_NOT_AUTHORIZED (VAL_A_FAIL_BASE+14)   /*The zone is using a key that is not authorized as per local policy. */
-#define VAL_A_ALGO_REFUSED (VAL_A_FAIL_BASE+15) /*Algorithm in DNSKEY or RRSIG or DS is not allowed as per local policy */
-#define VAL_A_NO_PREFERRED_SEP (VAL_A_FAIL_BASE+16)     /*There is no DNSKEY in the parent DS set that our local policy allows us to traverse */
-#define VAL_A_RRSIG_ALGO_MISMATCH (VAL_A_FAIL_BASE+17)  /* The DNSKEY and RRSIG pair have a mismatch in their algorithm. */
-#define VAL_A_LAST_FAILURE (VAL_A_FAIL_BASE+30) /* 80 */
+#define VAL_AC_DNSKEY_NOMATCH (VAL_AC_FAIL_BASE+2)        /*RRSIG was created by a DNSKEY that does not exist in the apex keyset. */
+#define VAL_AC_WRONG_LABEL_COUNT (VAL_AC_FAIL_BASE+3)     /*The number of labels on the signature is greater than the the count given in the RRSIG RDATA. */
+#define VAL_AC_BAD_DELEGATION (VAL_AC_FAIL_BASE+4) /*RRSIG created by a key that does not exist in the parent DS record set.*/
+#define VAL_AC_INVALID_KEY (VAL_AC_FAIL_BASE+5)        /*The key used to verify the RRSIG is not a zone key, or could not be parsed etc. */ 
+#define VAL_AC_INVALID_RRSIG (VAL_AC_FAIL_BASE+6)        /*The rrsig could not be parsed etc. */ 
+#define VAL_AC_RRSIG_NOTYETACTIVE (VAL_AC_FAIL_BASE+7)    /*The RRSIG's inception time is in the future. */
+#define VAL_AC_RRSIG_EXPIRED	(VAL_AC_FAIL_BASE+8)     /*The RRSIG has expired. */
+#define VAL_AC_ALGORITHM_NOT_SUPPORTED (VAL_AC_FAIL_BASE+9)    /* Algorithm in DNSKEY or RRSIG or DS is not supported. */
+#define VAL_AC_UNKNOWN_ALGORITHM (VAL_AC_FAIL_BASE+10)  /* Unknown DNSKEY or RRSIG or DS algorithm */
+#define VAL_AC_RRSIG_VERIFY_FAILED (VAL_AC_FAIL_BASE+11)  /*The RRSIG did not verify. */
+#define VAL_AC_KEY_TOO_LARGE (VAL_AC_FAIL_BASE+12)        /*The zone is using a key size that is too large as per local policy. */
+#define VAL_AC_KEY_TOO_SMALL (VAL_AC_FAIL_BASE+13)        /*The zone is using a key size that is too small as per local policy */
+#define VAL_AC_KEY_NOT_AUTHORIZED (VAL_AC_FAIL_BASE+14)   /*The zone is using a key that is not authorized as per local policy. */
+#define VAL_AC_ALGORITHM_REFUSED (VAL_AC_FAIL_BASE+15) /*Algorithm in DNSKEY or RRSIG or DS is not allowed as per local policy */
+#define VAL_AC_RRSIG_ALGORITHM_MISMATCH (VAL_AC_FAIL_BASE+17)  /* The DNSKEY and RRSIG pair have a mismatch in their algorithm. */
+#define VAL_AC_LAST_FAILURE (VAL_AC_FAIL_BASE+30) /* 80 */
 
 /*
  * success or unknown result conditions 
  */
-#define VAL_A_VERIFIED (VAL_A_LAST_FAILURE+1)   /* This is a transient state, it will settle at
+#define VAL_AC_VERIFIED (VAL_AC_LAST_FAILURE+1)   /* This is a transient state, it will settle at
                                                  * VALIDATED_SUCCESS if the */
 /* This signature status on success contains the following */
-#define VAL_A_RRSIG_VERIFIED (VAL_A_LAST_FAILURE+2)        /* The RRSIG verified successfully. */
-#define VAL_A_WCARD_VERIFIED (VAL_A_LAST_FAILURE+3)        /* The RRSIG verified successfully after wildcard expansion. */
-#define VAL_A_SIGNING_KEY (VAL_A_LAST_FAILURE+4) 
-#define VAL_A_VERIFIED_LINK (VAL_A_LAST_FAILURE+5)      /* This is a transient state, it will settle at VALIDATED_SUCCESS if the chain of trust can be completed */
-#define VAL_A_UNKNOWN_ALGO_LINK (VAL_A_LAST_FAILURE+6)      /* This is a transient state, it will settle at VALIDATED_SUCCESS if the chain of trust can be completed */
+#define VAL_AC_RRSIG_VERIFIED (VAL_AC_LAST_FAILURE+2)        /* The RRSIG verified successfully. */
+#define VAL_AC_WCARD_VERIFIED (VAL_AC_LAST_FAILURE+3)        /* The RRSIG verified successfully after wildcard expansion. */
+#define VAL_AC_SIGNING_KEY (VAL_AC_LAST_FAILURE+4) 
+#define VAL_AC_VERIFIED_LINK (VAL_AC_LAST_FAILURE+5)      /* This is a transient state, it will settle at VALIDATED_SUCCESS if the chain of trust can be completed */
+#define VAL_AC_UNKNOWN_ALGORITHM_LINK (VAL_AC_LAST_FAILURE+6)      /* This is a transient state, it will settle at VALIDATED_SUCCESS if the chain of trust can be completed */
 
-#define VAL_A_LOCAL_ANSWER (VAL_A_LAST_FAILURE+7)       /* Answer obtained locally */
-#define VAL_A_TRUST_KEY (VAL_A_LAST_FAILURE+8)  /* key is trusted */
-#define VAL_A_TRUST_ZONE (VAL_A_LAST_FAILURE+9) /* zone is trusted */
-#define VAL_A_PROVABLY_UNSECURE (VAL_A_LAST_FAILURE+10)
-#define VAL_A_BARE_RRSIG (VAL_A_LAST_FAILURE+11) /* No DNSSEC validation possible, query was for a RRSIG. */
-#define VAL_A_NO_TRUST_ANCHOR (VAL_A_LAST_FAILURE+12)    /* No trust anchor available, but components were verified */
+#define VAL_AC_LOCAL_ANSWER (VAL_AC_LAST_FAILURE+7)       /* Answer obtained locally */
+#define VAL_AC_TRUST_KEY (VAL_AC_LAST_FAILURE+8)  /* key is trusted */
+#define VAL_AC_TRUST_ZONE (VAL_AC_LAST_FAILURE+9) /* zone is trusted */
+#define VAL_AC_PROVABLY_UNSECURE (VAL_AC_LAST_FAILURE+10)
+#define VAL_AC_BARE_RRSIG (VAL_AC_LAST_FAILURE+11) /* No DNSSEC validation possible, query was for a RRSIG. */
+#define VAL_AC_NO_TRUST_ANCHOR (VAL_AC_LAST_FAILURE+12)    /* No trust anchor available, but components were verified */
 
 /*
  *************************************************** 
