8c05f7acbc8afc5159b29c021c86e7b9cf4a1309
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2006-11-15 22:07:13 +0000

    Move some of the libval-specific macros to val_support.h
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2323 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/validator.h
+++ dnssec-tools/validator/libval/validator.h
@@ -22,43 +22,6 @@ extern          "C" {
 #define NS_INT32SZ INT32SZ
 #endif
 
-#define VAL_GET16(s, cp) do { \
-        register const u_char *t_cp = (const u_char *)(cp); \
-        (s) = ((u_int16_t)t_cp[0] << 8) \
-            | ((u_int16_t)t_cp[1]) \
-            ; \
-        (cp) += NS_INT16SZ; \
-} while (0)
-
-#define VAL_GET32(l, cp) do { \
-        register const u_char *t_cp = (const u_char *)(cp); \
-        (l) = ((u_int32_t)t_cp[0] << 24) \
-            | ((u_int32_t)t_cp[1] << 16) \
-            | ((u_int32_t)t_cp[2] << 8) \
-            | ((u_int32_t)t_cp[3]) \
-            ; \
-        (cp) += NS_INT32SZ; \
-} while (0)
-
-#define CREATE_NSADDR_ARRAY(ns_address, len) do {\
-    int i, j;\
-    ns_address = (struct sockaddr_storage **) \
-        MALLOC (len * sizeof(struct sockaddr_storage *));\
-    if(ns_address == NULL) {\
-        return SR_MEMORY_ERROR;\
-    }\
-    for(i=0; i< len; i++) {\
-        ns_address[i] = (struct sockaddr_storage *) MALLOC (sizeof(struct sockaddr_storage));\
-        if (ns_address[i] == NULL) {\
-            for(j=0; j<i; j++) {\
-                FREE(ns_address[i]);\
-            }\
-            FREE(ns_address);\
-            ns_address = NULL;\
-        }\
-    }\
-}while(0)
-
 #if !defined(NS_PUT16) && defined(PUTSHORT)
 #define NS_PUT16 PUTSHORT
 #define NS_PUT32 PUTLONG
