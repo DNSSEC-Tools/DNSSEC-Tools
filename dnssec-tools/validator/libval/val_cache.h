5736c292f8421ee6e5e976aa13c96489b3e7fcc4
Suresh Krishnaswamy <hserus>
2006-11-20 19:32:49 +0000

    Maintain a separate cache for (zonename, server) mapping to account for the case where our default name server is authoritative for a non-public zone (such as example.com). Look up this cache before we look for other NS records. Rename get_matching_nslist to get_nslist_from_cache

--- dnssec-tools/validator/libval/val_cache.h
+++ dnssec-tools/validator/libval/val_cache.h
@@ -16,8 +16,10 @@ int             get_cached_rrset(u_int8_t * name_n, u_int16_t class_h,
                                  struct rrset_rec **cloned_answer);
 int             free_validator_cache(void);
 int             get_root_ns(struct name_server **ns);
-int             get_matching_nslist(struct val_query_chain *matched_q,
-                                    struct val_query_chain **queries,
-                                    struct name_server **ref_ns_list);
+int             store_ns_for_zone(u_int8_t *zonecut_n, 
+                                  struct name_server *resp_server); 
+int             get_nslist_from_cache(struct val_query_chain *matched_q,
+                                      struct val_query_chain **queries,
+                                      struct name_server **ref_ns_list);
 
 #endif
