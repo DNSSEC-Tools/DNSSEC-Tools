613826cd901d179bd148adcb0c7006518e4693e3
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2007-01-10 23:32:46 +0000

    Change prototypes for all the stow_ functions since we now do in-bailiwick checking Change prototypes for get_cached_rrset and get_nslist_from_cache as well.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2741 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_cache.h
+++ dnssec-tools/validator/libval/val_cache.h
@@ -6,20 +6,21 @@
 #define VAL_CACHE_H
 
 
-int             stow_zone_info(struct rrset_rec *new_info);
-int             stow_key_info(struct rrset_rec *new_info);
-int             stow_ds_info(struct rrset_rec *new_info);
+int             stow_zone_info(struct rrset_rec *new_info, struct val_query_chain *matched_q);
+int             stow_key_info(struct rrset_rec *new_info, struct val_query_chain *matched_q);
+int             stow_ds_info(struct rrset_rec *new_info, struct val_query_chain *matched_q);
+int             stow_answers(struct rrset_rec *new_info, struct val_query_chain *matched_q);
+int             stow_negative_answers(struct rrset_rec *new_info, struct val_query_chain *matched_q);
 int             stow_root_info(struct rrset_rec *root_info);
-int             stow_answer(struct rrset_rec *new_info);
-int             get_cached_rrset(u_int8_t * name_n, u_int16_t class_h,
-                                 u_int16_t type_h,
-                                 struct rrset_rec **cloned_answer);
+int             get_cached_rrset(struct val_query_chain *matched_q, struct domain_info **response);
 int             free_validator_cache(void);
 int             get_root_ns(struct name_server **ns);
 int             store_ns_for_zone(u_int8_t * zonecut_n,
                                   struct name_server *resp_server);
-int             get_nslist_from_cache(struct val_query_chain *matched_q,
+int             get_nslist_from_cache(val_context_t *ctx,
+                                      struct val_query_chain *matched_q,
                                       struct val_query_chain **queries,
-                                      struct name_server **ref_ns_list);
+                                      struct name_server **ref_ns_list,
+                                      u_int8_t **zonecut_n);
 
 #endif
