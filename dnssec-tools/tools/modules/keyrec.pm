b2fa16c55f87a5b8f2cc6a98d6d453ab2b9ce1aa
tewok <tewok@c4029f9c-b713-0410-a448-fae9e31a314a>
2007-01-19 02:09:25 +0000

    Change "kskpub" references to "ksknew". Add "ksknew" to the zone fields.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2807 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/tools/modules/keyrec.pm
+++ dnssec-tools/tools/modules/keyrec.pm
@@ -80,6 +80,7 @@ my @ZONEFIELDS = (
 			'kskcount',
 			'kskdirectory',
 			'kskcur',
+			'ksknew',
 			'lastset',
 			'signedzone',
 			'zskcount',
@@ -350,7 +351,7 @@ sub keyrec_read
 #
 #			key keyrecs:
 #				- ksk:
-#					Change to a kskcur, kskpub, or kskobs.
+#					Change to a kskcur, ksknew, or kskobs.
 #
 sub keyrec_fmtchk()
 {
@@ -412,7 +413,7 @@ sub keyrec_fmtchk()
 		# pointing to a signing set.  If not, create a new
 		# set and move the key there.
 		#
-		foreach my $key (qw /kskcur kskpub zskcur zskpub zsknew/)
+		foreach my $key (qw /kskcur ksknew zskcur zskpub zsknew/)
 		{
 			my $keyname;		# Key's name.
 			my $set;		# Signing set name.
@@ -469,7 +470,7 @@ sub keyrec_fmtchk()
 
 	#
 	# Check key keyrecs for problems:
-	#	- Change ksk type keyrecs to kskcur, kskpub, or kskobs.
+	#	- Change ksk type keyrecs to kskcur, ksknew, or kskobs.
 	#
 	foreach $krn (keyrec_names())
 	{
@@ -494,7 +495,7 @@ sub keyrec_fmtchk()
 		$krt = $krec{'keyrec_type'};
 
 		#
-		# Convert a ksk keyrec into either a kskcur, kskpub,
+		# Convert a ksk keyrec into either a kskcur, ksknew,
 		# or kskobs keyrec.
 		#
 		if($krt eq 'ksk')
@@ -508,7 +509,7 @@ sub keyrec_fmtchk()
 			# any of the zone's key sets.
 			#
 			$zone = $krec{'zonename'};
-			foreach my $key (qw /kskcur kskpub/)
+			foreach my $key (qw /kskcur ksknew/)
 			{
 				$set = $keyrecs{$zone}{$key};
 				if(keyrec_signset_haskey($set,$krn))
