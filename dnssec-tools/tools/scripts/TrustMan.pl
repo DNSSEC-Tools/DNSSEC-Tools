182a5536d66c9ece9b3f936d34cb17064f0911af
Wes Hardaker <hardaker>
2007-01-31 03:47:48 +0000

    DNSSEC-Tools version update

--- dnssec-tools/tools/scripts/TrustMan.pl
+++ dnssec-tools/tools/scripts/TrustMan.pl
@@ -131,7 +131,7 @@ if ($opts{'f'}) {
 
 sub show_version {
     print STDERR "Version: 0.7\n";
-    print STDERR "DNSSEC-Tools Version: 1.0\n";
+    print STDERR "DNSSEC-Tools Version: NEWVERSION\n";
     exit(1);
 }
 
