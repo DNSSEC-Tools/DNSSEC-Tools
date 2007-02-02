1487c1dddf0a6d817680d2d3e1d03da68899d044
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2007-02-02 15:54:15 +0000

    Define a new context-creation function that allows you to specify the resolver/validator/root-hints configuration files Perform initialization and destruction of new context members that implement config refresh functionality Define new functions to refresh configuration files
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2880 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_context.c
+++ dnssec-tools/validator/libval/val_context.c
@@ -18,7 +18,11 @@
 #include "val_assertion.h"
 
 int
-val_create_context(char *label, val_context_t ** newcontext)
+val_create_context_with_conf(char *label, 
+                             char *dnsval_conf, 
+                             char *resolv_conf, 
+                             char *root_conf, 
+                             val_context_t ** newcontext)
 {
     int             retval;
 
@@ -29,14 +33,52 @@ val_create_context(char *label, val_context_t ** newcontext)
     if (*newcontext == NULL)
         return VAL_OUT_OF_MEMORY;
 
-    (*newcontext)->q_list = NULL;
-    (*newcontext)->a_list = NULL;
-
     if (snprintf
         ((*newcontext)->id, VAL_CTX_IDLEN - 1, "%u",
          (unsigned) (*newcontext)) < 0)
         strcpy((*newcontext)->id, "libval");
 
+    if (label){
+        (*newcontext)->label = (char *) MALLOC (strlen(label) + 1);
+        if ((*newcontext)->label == NULL) {
+            FREE(*newcontext);
+            *newcontext = NULL;
+            return VAL_OUT_OF_MEMORY;
+        }
+    } else {
+        (*newcontext)->label = NULL;
+    }
+
+    /* 
+     * Set default configuration files 
+     */
+    (*newcontext)->dnsval_conf = dnsval_conf? strdup(dnsval_conf) : dnsval_conf_get(); 
+    (*newcontext)->resolv_conf = resolv_conf? strdup(resolv_conf) : resolv_conf_get(); 
+    (*newcontext)->root_conf = root_conf? strdup(root_conf) : root_hints_get(); 
+
+    memset(&(*newcontext)->r_timestamp, 0, sizeof(struct timespec));
+    memset(&(*newcontext)->v_timestamp, 0, sizeof(struct timespec));
+    memset(&(*newcontext)->h_timestamp, 0, sizeof(struct timespec));
+
+    (*newcontext)->root_ns = NULL; 
+
+    (*newcontext)->nslist = NULL; 
+
+    (*newcontext)->e_pol =
+        (policy_entry_t *) MALLOC(MAX_POL_TOKEN * sizeof(policy_entry_t));
+    if ((*newcontext)->e_pol == NULL) {
+        FREE(*newcontext);
+        *newcontext = NULL;
+        return VAL_OUT_OF_MEMORY;
+    }
+    memset((*newcontext)->e_pol, 0,
+           MAX_POL_TOKEN * sizeof(policy_entry_t));
+    (*newcontext)->pol_overrides = NULL;
+    (*newcontext)->cur_override = NULL;
+   
+    /*
+     * Read the Root Hints file; has to be read before resolver config file 
+     */
     if ((retval = read_root_hints_file(*newcontext)) != VAL_NO_ERROR) {
         FREE(*newcontext);
         *newcontext = NULL;
@@ -55,19 +97,6 @@ val_create_context(char *label, val_context_t ** newcontext)
     /*
      * Read the validator configuration file 
      */
-    (*newcontext)->e_pol =
-        (policy_entry_t *) MALLOC(MAX_POL_TOKEN * sizeof(policy_entry_t));
-    if ((*newcontext)->e_pol == NULL) {
-        destroy_respol(*newcontext);
-        FREE(*newcontext);
-        *newcontext = NULL;
-        return VAL_OUT_OF_MEMORY;
-    }
-
-    memset((*newcontext)->e_pol, 0,
-           MAX_POL_TOKEN * sizeof(policy_entry_t));
-    (*newcontext)->pol_overrides = NULL;
-    (*newcontext)->cur_override = NULL;
     if ((retval =
          read_val_config_file(*newcontext, label)) != VAL_NO_ERROR) {
         destroy_respol(*newcontext);
@@ -75,17 +104,45 @@ val_create_context(char *label, val_context_t ** newcontext)
         *newcontext = NULL;
         return retval;
     }
+    OVERRIDE_POLICY((*newcontext));
+
+    /* 
+     * Initialize caches 
+     */
+    (*newcontext)->q_list = NULL;
+    (*newcontext)->a_list = NULL;
 
     return VAL_NO_ERROR;
 }
 
+int
+val_create_context(char *label, 
+                   val_context_t ** newcontext)
+{
+    return val_create_context_with_conf(label, NULL, NULL, NULL, newcontext);
+}
 
 void
 val_free_context(val_context_t * context)
 {
-    if (context == NULL)
+    if ((context == NULL) || (context == the_default_context))
         return;
 
+    if (context->label)
+        FREE(context->label);
+
+    if (context->dnsval_conf)
+        FREE(context->dnsval_conf);
+
+    if (context->resolv_conf)
+        FREE(context->resolv_conf);
+
+    if (context->root_conf)
+        FREE(context->root_conf);
+
+    if (context->root_ns)
+        free_name_servers(&context->root_ns);
+    
     destroy_respol(context);
     destroy_valpol(context);
     FREE(context->e_pol);
@@ -95,3 +152,53 @@ val_free_context(val_context_t * context)
 
     FREE(context);
 }
+
+void 
+val_refresh_resolver_policy(val_context_t * context)
+{
+    if ((context == NULL) || (context == the_default_context))
+        return;
+
+    if (read_res_config_file(context) != VAL_NO_ERROR) {
+        context->r_timestamp = -1;
+        val_log(context, LOG_WARNING, "Resolver configuration could not be read; using older values");
+        return; 
+    }
+}
+
+void 
+val_refresh_validator_policy(val_context_t * context)
+{
+    if ((context == NULL) || (context == the_default_context))
+        return;
+
+    if (read_val_config_file(context, context->label) != VAL_NO_ERROR) {
+        val_log(context, LOG_WARNING, "Resolver configuration could not be read; using older values");
+        context->v_timestamp = -1;
+        return; 
+    }
+    OVERRIDE_POLICY((context));
+
+    /* 
+     * Re-initialize caches 
+     */
+    free_query_chain(context->q_list);
+    free_authentication_chain(context->a_list);
+
+    context->q_list = NULL;
+    context->a_list = NULL;
+}
+
+void 
+val_refresh_root_hints(val_context_t * context)
+{
+    if ((context == NULL) || (context == the_default_context))
+        return;
+
+    if (read_root_hints_file(context) != VAL_NO_ERROR) {
+        val_log(context, LOG_WARNING, "Root Hints could not be read; using older values");
+        context->h_timestamp = -1;
+        return; 
+    }
+}
+
