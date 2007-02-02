80530580ab087ae706b0040185214533242daba2
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2007-02-02 15:54:48 +0000

    Define prototypes of new functions that allow refresh of configuration files to be possible
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2881 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/libval/val_context.h
+++ dnssec-tools/validator/libval/val_context.h
@@ -5,8 +5,16 @@
 #ifndef VAL_CONTEXT_H
 #define VAL_CONTEXT_H
 
+int             val_create_context_with_conf(char *label,
+                                             char *dnsval_conf,
+                                             char *resolv_conf,
+                                             char *root_conf,
+                                             val_context_t ** newcontext);
 int             val_create_context(char *label,
                                    val_context_t ** newcontext);
 void            val_free_context(val_context_t * context);
+void            val_refresh_resolver_policy(val_context_t * context);
+void            val_refresh_validator_policy(val_context_t * context);
+void            val_refresh_root_hints(val_context_t * context);
 
 #endif
