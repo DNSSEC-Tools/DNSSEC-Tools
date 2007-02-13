d0a1b1a059120a29bcc9f26084e836bbc555fb1d
rstory <rstory@c4029f9c-b713-0410-a448-fae9e31a314a>
2007-02-13 18:12:14 +0000

    new header
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@2915 c4029f9c-b713-0410-a448-fae9e31a314a

---
+++ dnssec-tools/validator/apps/validator_driver.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2007 SPARTA, Inc.  All rights reserved.
+ * See the COPYING file distributed with this software for details.
+ */
+#ifndef VALIDATOR_DRIVER_H
+#define VALIDATOR_DRIVER_H
+
+void print_val_response(struct val_response *resp);
+int sendquery(val_context_t * context, const char *desc, u_char * name_n,
+              const u_int16_t class, const u_int16_t type,
+              const int *result_ar, int trusted_only,
+              struct val_response **resp);
+
+int self_test(val_context_t *context, int tcs, int tce, char *label_str,
+              int doprint);
+
+
+
+#endif /* VALIDATOR_DRIVER_H */
