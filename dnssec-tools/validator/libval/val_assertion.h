3d7a01ffb88d7fc00c3b7159efd7c0dcf8f00f5a
Suresh Krishnaswamy <hserus>
2007-02-27 17:55:11 +0000

    - Modified function prototypes: use the queries_for_query linked   list instead of the val_query_chain linked list while tracking   queries issued for val_resolve_and_check()

--- dnssec-tools/validator/libval/val_assertion.h
+++ dnssec-tools/validator/libval/val_assertion.h
@@ -7,7 +7,15 @@
 
 int             add_to_query_chain(struct val_query_chain **queries,
                                    u_char * name_n, const u_int16_t type_h,
-                                   const u_int16_t class_h);
+                                   const u_int16_t class_h, 
+                                   const u_int8_t flags,
+                                   struct val_query_chain **added_q);
+int             add_to_qfq_chain(val_context_t *context,
+                                 struct queries_for_query **queries,
+                                 u_char * name_n, const u_int16_t type_h,
+                                 const u_int16_t class_h, 
+                                 const u_int8_t flags,
+                                 struct queries_for_query **added_q);
 void            free_authentication_chain(struct val_digested_auth_chain
                                           *assertions);
 void            free_query_chain(struct val_query_chain *queries);
@@ -15,6 +23,14 @@ int             val_istrusted(val_status_t val_status);
 int             val_isvalidated(val_status_t val_status);
 u_int16_t       is_trusted_zone(val_context_t * ctx, u_int8_t * name_n);
 void            val_free_result_chain(struct val_result_chain *results);
+int             try_chase_query(val_context_t * context,
+                                u_char * domain_name_n,
+                                const u_int16_t q_class,
+                                const u_int16_t type,
+                                const u_int8_t flags,
+                                struct queries_for_query **queries,
+                                struct val_result_chain **results,
+                                int *done);
 int             val_resolve_and_check(val_context_t * context,
                                       u_char * domain_name,
                                       const u_int16_t class,
