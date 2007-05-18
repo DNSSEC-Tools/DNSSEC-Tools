3895469f8d6bf9e05745cc047221cab5fa2cee6a
Suresh Krishnaswamy <hserus>
2007-05-18 18:34:38 +0000

    Updated date on copyright notice. Moved the fix_glue function from val_assertion.c to val_resquery.c Removed definitions for some static functions

--- dnssec-tools/validator/libval/val_resquery.h
+++ dnssec-tools/validator/libval/val_resquery.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 SPARTA, Inc.  All rights reserved.
+ * Copyright 2005-2007 SPARTA, Inc.  All rights reserved.
  * See the COPYING file distributed with this software for details.
  */
 #ifndef VAL_RESQUERY_H
@@ -11,33 +11,30 @@ struct glue_fetch_bucket {
     struct glue_fetch_bucket *next_bucket;
 };
 
-void            free_referral_members(struct delegation_info *del);
+int             fix_glue(val_context_t * context,
+                         struct queries_for_query **queries,
+                         int *data_missing);
 int             res_zi_unverified_ns_list(struct name_server **ns_list,
                                           u_int8_t * zone_name,
                                           struct rrset_rec
                                           *unchecked_zone_info, struct name_server
                                           **pending_glue);
+int             find_nslist_for_query(val_context_t * context,
+                                      struct queries_for_query *next_qfq,
+                                      struct queries_for_query **queries);
 int             bootstrap_referral(val_context_t *context,
                                    u_int8_t * referral_zone_n,
                                    struct rrset_rec **learned_zones,
                                    struct queries_for_query *matched_qfq,
                                    struct queries_for_query **queries,
                                    struct name_server **ref_ns_list);
+void            free_referral_members(struct delegation_info *del);
 int             process_cname_dname_responses(u_int8_t *name_n, 
                               u_int16_t type_h, 
                               u_int8_t *rdata, 
                               struct val_query_chain *matched_q,
                               struct qname_chain **qnames,
                               int *referral_error);
-int             extract_glue_from_rdata(struct rr_rec *addr_rr,
-                                        struct name_server **ns);
-int             merge_glue_in_referral(val_context_t *context,
-                                       struct queries_for_query *qfq_pc,
-                                       struct glue_fetch_bucket *bucket,
-                                       struct queries_for_query **queries);
-int             find_nslist_for_query(val_context_t * context,
-                                      struct queries_for_query *next_qfq,
-                                      struct queries_for_query **queries);
 int             val_resquery_send(val_context_t * context,
                                   struct queries_for_query *matched_qfq);
 int             val_resquery_rcv(val_context_t * context,
