8f2b6f8eeb90ef27aba231af04b52a6ea8bc018c
Suresh Krishnaswamy <hserus>
2007-10-04 19:29:53 +0000

    Use select if pselect is not available

--- dnssec-tools/validator/libsres/res_io_manager.c
+++ dnssec-tools/validator/libsres/res_io_manager.c
@@ -607,8 +607,11 @@ res_io_select_sockets(fd_set * read_descriptors, struct timeval *timeout)
     timeout_ts.tv_sec = timeout->tv_sec;
     timeout_ts.tv_nsec = 0;
 
-    //return select(max_sock + 1, read_descriptors, NULL, NULL, timeout);
+#ifdef HAVE_PSELECT
     return pselect(max_sock + 1, read_descriptors, NULL, NULL, &timeout_ts, NULL);
+#else
+    return select(max_sock + 1, read_descriptors, NULL, NULL, timeout);
+#endif
 }
 
 void
