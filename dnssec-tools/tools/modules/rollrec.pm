156955f131dc8f1008b2f1d261779708da902f19
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2009-02-06 00:57:50 +0000

    make makelocalstatedir accept a subdir argument
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@4349 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/tools/modules/rollrec.pm
+++ dnssec-tools/tools/modules/rollrec.pm
@@ -151,7 +151,7 @@ sub rollrec_lock
 	#
 	# Get the DNSSEC-Tools config directory.
 	#
-	$lockdir = makelocalstatedir() . "/dnssec-tools"
+	$lockdir = makelocalstatedir("/dnssec-tools")
 	  || $DEFAULT_DNSSECTOOLS_DIR;
 
 	#
