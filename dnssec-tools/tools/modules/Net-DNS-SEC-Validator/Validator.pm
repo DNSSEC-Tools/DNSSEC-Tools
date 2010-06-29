8505e8b0991b1904d0a53f36edf94fdfd69b7e4c
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2010-06-29 17:00:52 +0000

    Bump the version number
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5128 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/tools/modules/Net-DNS-SEC-Validator/Validator.pm
+++ dnssec-tools/tools/modules/Net-DNS-SEC-Validator/Validator.pm
@@ -16,7 +16,7 @@ require Net::hostent; # return type from gethost*
 use Net::DNS; # to interpret DNS classes and types
 use Carp;
 
-our $VERSION = '1.31';   # current release version number
+our $VERSION = '1.40';   # current release version number
 
 use Exporter;
 use DynaLoader;
