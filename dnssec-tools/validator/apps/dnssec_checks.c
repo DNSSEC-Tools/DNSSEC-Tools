b6dd17c6789951b7121a34dec7e19168d3ad1ddd
Wes Hardaker <hardaker>
2011-03-23 20:12:47 +0000

    - return error strings with failures - fix tests to correct them

--- dnssec-tools/validator/apps/dnssec_checks.c
+++ dnssec-tools/validator/apps/dnssec_checks.c
@@ -16,7 +16,6 @@
 #define CHECK_SUCCEEDED 0
 #define CHECK_FAILED    1
 
-
 #define RETURN_ERROR_BUF(msg, buffer, buffer_len)          \
     do {                                                   \
         strncpy(buffer, msg, buffer_len-1);                \
