6fec531890d9e5b9ce2902f430a43ae292eee6ef
Wes Hardaker <hardaker>
2011-09-23 17:53:02 +0000

    adding the startings of a settings window

--- dnssec-tools/validator/apps/dnssec-nodes/NodesPreferences.h
+++ dnssec-tools/validator/apps/dnssec-nodes/NodesPreferences.h
@@ -2,17 +2,29 @@
 #define NODESPREFERENCES_H
 
 #include <QDialog>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QFormLayout>
+#include <QtGui/QSpinBox>
+#include <QtCore/QSettings>
 
 class NodesPreferences : public QDialog
 {
     Q_OBJECT
 public:
-    explicit NodesPreferences(QWidget *parent = 0);
+    explicit NodesPreferences(QSettings &settings, QWidget *parent = 0);
+    int maxNodeCount();
 
 signals:
 
 public slots:
+    void ok();
 
+private:
+    QVBoxLayout *m_vbox;
+    QFormLayout *m_layout;
+    QSpinBox    *m_maxNodes;
+
+     QSettings  &m_settings;
 };
 
 #endif // NODESPREFERENCES_H
