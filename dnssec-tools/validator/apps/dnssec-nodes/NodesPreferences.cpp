8917da42c5cc89c11c0e8c367b065bac3d73ee4c
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2011-09-23 17:53:02 +0000

    adding the startings of a settings window
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5818 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-nodes/NodesPreferences.cpp
+++ dnssec-tools/validator/apps/dnssec-nodes/NodesPreferences.cpp
@@ -1,7 +1,32 @@
 #include "NodesPreferences.h"
 
-NodesPreferences::NodesPreferences(QWidget *parent) :
-    QDialog(parent)
-{
+#include <QtGui/QDialogButtonBox>
 
+NodesPreferences::NodesPreferences(QSettings &settings, QWidget *parent) :
+    QDialog(parent), m_settings(settings)
+{
+    m_vbox = new QVBoxLayout();
+    m_layout = new QFormLayout();
+    m_vbox->addLayout(m_layout);
+    setLayout(m_vbox);
+
+    m_maxNodes = new QSpinBox();
+    m_layout->addRow("Max Nodes: ", m_maxNodes);
+    m_maxNodes->setMaximum(0xffff);
+    m_maxNodes->setMinimum(1);
+    m_maxNodes->setValue(m_settings.value("maxNodes", 0).toInt());
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    m_vbox->addWidget(buttonBox);
+
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(ok()));
+}
+
+void NodesPreferences::ok() {
+    m_settings.setValue("maxNodes", m_maxNodes->value());
+    accept();
+}
+
+int NodesPreferences::maxNodeCount() {
+    return m_maxNodes->value();
 }
