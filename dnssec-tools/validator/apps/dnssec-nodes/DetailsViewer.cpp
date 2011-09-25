50485b647142e5da03030ad42afd021ea505766d
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2011-09-25 15:12:03 +0000

    Put the log details into a tab.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5842 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-nodes/DetailsViewer.cpp
+++ dnssec-tools/validator/apps/dnssec-nodes/DetailsViewer.cpp
@@ -3,14 +3,26 @@
 #include <QtGui/QLabel>
 #include <QtGui/QTextEdit>
 #include <QtGui/QFont>
+#include <QtGui/QTabWidget>
 
 #include <qdebug.h>
 
 DetailsViewer::DetailsViewer(Node *node, QWidget *parent) :
     QDialog(parent), m_node(node)
 {
+
     m_layout = new QVBoxLayout();
     setLayout(m_layout);
+    QTabWidget *tabs = new QTabWidget();
+    m_layout->addWidget(tabs);
+
+    //
+    // Log Message Viewer
+    //
+    QWidget *widget = new QWidget();
+    QVBoxLayout *vbox = new QVBoxLayout();
+
+    widget->setLayout(vbox);
 
     QLabel *title = new QLabel(node->fqdn(), this);
     QFont font = title->font();
@@ -18,17 +30,24 @@ DetailsViewer::DetailsViewer(Node *node, QWidget *parent) :
     font.setUnderline(true);
     font.setPointSize(16);
     title->setFont(font);
-    m_layout->addWidget(title);
+    vbox->addWidget(title);
 
     QTextEdit *textEdit = new QTextEdit("<p>" + node->logMessages().join("</p><p>") + "</p>", this);
     textEdit->setReadOnly(true);
     textEdit->setLineWrapMode(QTextEdit::NoWrap);
-    m_layout->addWidget(textEdit);
+    vbox->addWidget(textEdit);
 
-    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok, Qt::Horizontal, this);
+    tabs->addTab(widget, tr("Log Messages"));
+
+
+    //
+    // closing button box
+    //
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Close, Qt::Horizontal, this);
     m_layout->addWidget(buttonBox);
 
     setMinimumSize(600,400);
 
-    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(accept()));
 }
