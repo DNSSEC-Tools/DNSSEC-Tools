4ffd96b16d4818b0beebbb78127386ed79416741
rstory <rstory@c4029f9c-b713-0410-a448-fae9e31a314a>
2011-09-30 02:44:09 +0000

    define timersub macro if not defined
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5874 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/include/validator/resolver.h
+++ dnssec-tools/validator/include/validator/resolver.h
@@ -16,6 +16,41 @@
  * WITH THE USE OR PERFORMANCE OF THE SOFTWARE.
  */
 /*
+ * The timersub macro is from
+ *      $NetBSD: time.h,v 1.64 2009/03/27 11:06:26 drochner Exp $ 
+ * which specifies the following license:
+ *
+ * Copyright (c) 1982, 1986, 1993
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *      @(#)time.h      8.5 (Berkeley) 5/4/95
+ */
+
+/*
  * Copyright 2005-2011 SPARTA, Inc.  All rights reserved.
  * See the COPYING file distributed with this software for details.
  */
@@ -238,6 +273,23 @@ res_async_ea_is_using_stream(struct expected_arrival *ea);
 
 int
 res_async_ea_isset(struct expected_arrival *ea, fd_set *fds);
+
+/*
+ * define timersub macro if not defined. Set top of this header file
+ * for the license for this hunk of code.
+ */
+#ifndef timersub
+#define timersub(tvp, uvp, vvp)                                         \
+        do {                                                            \
+                (vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;          \
+                (vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec;       \
+                if ((vvp)->tv_usec < 0) {                               \
+                        (vvp)->tv_sec--;                                \
+                        (vvp)->tv_usec += 1000000;                      \
+                }                                                       \
+        } while (/* CONSTCOND */ 0)
+#endif
+
 
 #ifdef __cplusplus
 }                               /* extern C */
