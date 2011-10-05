1af092dc0f54338f544c3048cd280cc8a346d4be
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2011-10-05 18:29:15 +0000

    Added a status type menu
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5927 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-nodes/Filters/DNSSECStatusFilter.h
+++ dnssec-tools/validator/apps/dnssec-nodes/Filters/DNSSECStatusFilter.h
@@ -4,17 +4,32 @@
 #include "Filter.h"
 #include "node.h"
 
+#include <QtCore/QSignalMapper>
+#include <QtGui/QPushButton>
+
 class DNSSECStatusFilter : public Filter
 {
+    Q_OBJECT
+
 public:
     DNSSECStatusFilter(int dnssecValidity, bool requireAll = true);
 
+
     virtual bool      matches(Node *node);
     virtual QString   name() { return "DNSSEC Status Filter"; }
+    virtual void      configWidgets(QHBoxLayout *hbox);
+
+public slots:
+    virtual void      setDNSSECValidity(int dnssecValidity);
+    virtual void      setDNSSECValidityName(QString name);
 
 private:
     int               m_dnssecValidity;
     bool              m_requireAll;
+
+    QSignalMapper     m_mapper;
+    QPushButton      *m_menuButton;
+    QMap<DNSData::Status, QString> m_validityType;
 };
 
 #endif // DNSSECSTATUSFILTER_H
