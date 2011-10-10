cb21588afb1797b34857975512164783a0ee27ea
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2011-10-10 21:14:45 +0000

    add an optional short-location-message field for submitting data
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5985 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-check/SubmitDialog.cpp
+++ dnssec-tools/validator/apps/dnssec-check/SubmitDialog.cpp
@@ -10,6 +10,11 @@ SubmitDialog::SubmitDialog(QWidget *parent) :
     topLayout = new QVBoxLayout();
     topLayout->addWidget(new QLabel("Would you like to submit the results to the public database?"));
 
+    topLayout->addWidget(new QLabel("(optional) A short description of your location:"));
+    m_locationDescription = new QLineEdit();
+    m_locationDescription->setMaxLength(255);
+    topLayout->addWidget(m_locationDescription);
+
     buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok |
                                      QDialogButtonBox::Cancel,
                                      Qt::Horizontal, this);
@@ -17,6 +22,12 @@ SubmitDialog::SubmitDialog(QWidget *parent) :
     connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
     topLayout->addWidget(buttonBox);
 
+
     setLayout(topLayout);
+    m_locationDescription->setFocus();
 }
 
+QString
+SubmitDialog::locationDescription() {
+    return m_locationDescription->text();
+}
--- dnssec-tools/validator/apps/dnssec-check/SubmitDialog.h
+++ dnssec-tools/validator/apps/dnssec-check/SubmitDialog.h
@@ -3,18 +3,21 @@
 
 #include <QDialog>
 #include <QtGui/QVBoxLayout>
+#include <QtGui/QLineEdit>
 
 class SubmitDialog : public QDialog
 {
     Q_OBJECT
 public:
     explicit SubmitDialog(QWidget *parent = 0);
+    QString locationDescription();
 
 signals:
 
 public slots:
 private:
     QVBoxLayout *topLayout;
+    QLineEdit   *m_locationDescription;
 };
 
 #endif // SUBMITDIALOG_H
