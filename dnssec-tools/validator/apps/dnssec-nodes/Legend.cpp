208097c57f55f6c9dee5799d8e04e16efb12c21e
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2011-10-11 20:55:30 +0000

    Initial mostly working legend.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@5996 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-nodes/Legend.cpp
+++ dnssec-tools/validator/apps/dnssec-nodes/Legend.cpp
@@ -3,6 +3,12 @@
 #include <QtGui/QVBoxLayout>
 #include <QtGui/QLabel>
 #include <QtGui/QDialogButtonBox>
+#include <QTableWidget>
+
+#include <qdebug.h>
+
+#include "DNSData.h"
+#include "node.h"
 
 Legend::Legend(QWidget *parent) :
     QDialog(parent)
@@ -11,8 +17,31 @@ Legend::Legend(QWidget *parent) :
     QVBoxLayout *layout = new QVBoxLayout();
     setLayout(layout);
 
+    DNSData d;
+    Node *n = new Node(0);
+
     layout->addWidget(label = new QLabel("<h2>Legend</h2>"));
     label->setAlignment(Qt::AlignHCenter);
+
+    QList<DNSData::Status> statuses;
+    statuses << DNSData::UNKNOWN <<  DNSData::TRUSTED <<  DNSData::VALIDATED <<  DNSData::DNE << DNSData::FAILED <<  DNSData::IGNORE;
+
+    // Add the legend widget
+    QTableWidget *table = new QTableWidget(statuses.count(), 2, this);
+    layout->addWidget(table);
+
+
+    int row = 0;
+    foreach(DNSData::Status status, statuses) {
+        QTableWidgetItem *item = new QTableWidgetItem(d.DNSSECStatusForEnum(status));
+        item->setFlags(Qt::ItemIsEnabled);
+        item->setBackgroundColor(n->getColorForStatus(status).lighter());
+        table->setItem(row, 0, item);
+        row++;
+    }
+    table->setRowCount(row);
+    table->setColumnCount(1);
+    table->resizeColumnsToContents();
 
     //
     // closing button box
