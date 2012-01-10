0550b492200fba67a8fdb216b8452e79c7cce6e5
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2012-01-10 23:25:42 +0000

    added documentation to the header about what it defines, add symbian
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@6238 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-check/whatami.h
+++ dnssec-tools/validator/apps/dnssec-check/whatami.h
@@ -1,3 +1,11 @@
+/*
+ * This header creates two useful definitions for using in C++ code:
+ *
+ * USE_QML:        defined to 1 if being compiled for a meego, android or symbian device
+ *                 (these devices, in theory, look better when using a qml interface)
+ * SMALL_DEVICE:   defined to 1 if on an android, meego, maemo5 or symbian device
+ */
+
 #if defined(Q_WS_MAEMO_5) || defined(MAEMO_CHANGES)
 #define IS_MAEMO 1
 #endif
@@ -6,6 +14,22 @@
 #define IS_MEEGO 1
 #endif
 
-#ifdef IS_MEEGO
+#if defined(ANDROID)
+#define IS_ANDROID 1
+#endif
+
+#if defined(Q_OS_SYMBIAN)
+#define IS_SYMBIAN 1
+#endif
+
+/* define whether or not we're on a "small screen" device, which should have a more
+ * limited interface.  This decision is highly subjective and you may wish to tune it
+ * according to local desire. */
+#if defined(IS_MEEGO) || defined(ANDROID) || defined(Q_WS_MAEMO_5) || defined(IS_SYMBIAN)
+#define SMALL_DEVICE 1
+#endif
+
+
+#if defined(IS_MEEGO) || defined(ANDROID) || defined(IS_SYMBIAN)
 #define USE_QML 1
 #endif
