9454f4fa1cf3d8eab598c5afbf6beebf4c533668
tewok <tewok@c4029f9c-b713-0410-a448-fae9e31a314a>
2012-02-20 19:04:42 +0000

    This diff contains the mods for DNSSEC-Tools-enhanced version of status.c. The diff is required, as opposed to the actual file, due to copyright considerations.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@6485 c4029f9c-b713-0410-a448-fae9e31a314a

---
+++ dnssec-tools/apps/nagios/diff-status.c
@@ -0,0 +1,43 @@
+80a81,85
+> /*
+>  * Defines used by the DNSSEC-Tools modifications.
+>  */
+> #define	ROLL_STATUS	"Zone Rollover"
+> 
+1253a1259
+> 	char *dt_rollstatus="";			/* DNSSEC-Tools	*/
+1521a1528
+> 				dt_rollstatus = "PENDING";    /* DNSSEC_Tools */
+1526a1534,1545
+> 
+> 				/* DNSSEC_Tools */
+> 				if((strncmp(temp_status->plugin_output,"KSK Rollover",12) == 0) ||
+> 				   (strncmp(temp_status->plugin_output,"ZSK Rollover",12) == 0))
+> 				{
+> 					dt_rollstatus = "ROLLING";
+> 				}
+> 				else
+> 				{
+> 					dt_rollstatus = "NORMAL";
+> 				}
+> 
+1536a1556
+> 				dt_rollstatus = "ATTENTION REQUIRED";    /* DNSSEC_Tools */
+1546a1567
+> 				dt_rollstatus = "UNKNOWN";    /* DNSSEC_Tools */
+1556a1578
+> 				dt_rollstatus = "ATTENTION REQUIRED";    /* DNSSEC_Tools */
+1560d1581
+< 
+1786c1807,1815
+< 			printf("<TD CLASS='status%s'>%s</TD>\n",status_class,status);
+---
+> 			if(strcmp(temp_status->description,ROLL_STATUS) != 0)
+> 			{
+> 				printf("<TD CLASS='status%s'>%s</TD>\n",status_class,status);
+> 			}
+> 			else
+> 			{
+>                         	/* DNSSEC_Tools */
+> 				printf("<TD CLASS='status%s'>%s</TD>\n",status_class,dt_rollstatus);
+> 			}
