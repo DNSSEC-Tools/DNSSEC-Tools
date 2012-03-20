75d472b97578869ba899be4209b8424109cb1518
Suresh Krishnaswamy <hserus>
2012-03-20 18:31:13 +0000

    Add missing semicolon

--- dnssec-tools/validator/libval/val_inline_conf.h
+++ dnssec-tools/validator/libval/val_inline_conf.h
@@ -2,11 +2,11 @@
 #define VAL_INLINE_CONF_H
 
 #ifdef LIBVAL_INLINE_POLICY
-const char val_conf_inline_buf[] = ""
+const char val_conf_inline_buf[] = "";
 
-const char root_hints_inline_buf[] = ""
+const char root_hints_inline_buf[] = "";
 
-const char resolv_conf_inline_buf[] = ""
+const char resolv_conf_inline_buf[] = "";
 #else
 const char val_conf_inline_buf[] = "";
 const char root_hints_inline_buf[] = "";
