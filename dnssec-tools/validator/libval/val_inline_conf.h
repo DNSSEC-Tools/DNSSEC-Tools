11d4c9db8fbb5a56c9e4bdfb55b9fae728c1ce29
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2012-03-20 17:15:10 +0000

    Provide default validator-config.h and val_inline_conf.h files for environments where running the configure script may not be possible.
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@6573 c4029f9c-b713-0410-a448-fae9e31a314a

---
+++ dnssec-tools/validator/libval/val_inline_conf.h
@@ -0,0 +1,16 @@
+#ifndef VAL_INLINE_CONF_H
+#define VAL_INLINE_CONF_H
+
+#ifdef LIBVAL_INLINE_POLICY
+const char val_conf_inline_buf[] = ""
+
+const char root_hints_inline_buf[] = ""
+
+const char resolv_conf_inline_buf[] = ""
+#else
+const char val_conf_inline_buf[] = "";
+const char root_hints_inline_buf[] = "";
+const char resolv_conf_inline_buf[] = "";
+#endif
+
+#endif
