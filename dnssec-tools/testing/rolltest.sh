5fc1123802f1f355255f1a45dedef1fc4bc07eae
Wes Hardaker <hardaker>
2012-04-18 21:10:59 +0000

    rework it so it doesn't call zonesigner for older versions

--- dnssec-tools/testing/rolltest.sh
+++ dnssec-tools/testing/rolltest.sh
@@ -5,6 +5,13 @@ echo $curdir
 dir=/tmp/rolltest
 rmdir=1
 rrfile=dns.rollrec
+ZONEVERSION=`zonesigner 2>&1 -Version | grep "Tools Version" | sed 's/.*: //'`
+if [ $ZONEVERSION = '1.12' -o  $ZONEVERSION = '1.12.1' ] ; then
+    SKIPZONESIGNER=1
+else
+    SKIPZONESIGNER=0
+fi
+
 ZONESIGNER="zonesigner -verbose -verbose -verbose"
 ROLLERD="rollerd -singlerun -rrfile $rrfile -dtconf dnssec-tools.conf -logfile - -loglevel tmi"
 
@@ -99,9 +106,10 @@ checkrollerdphase 0
 checkkeyid $curkeyid rollerd changed the zsk immediately...  should have waited 60
 
 # check that zonesigner does the right thing and uses the current key too
-$ZONESIGNER example.com
-checkkeyid $curkeyid zonesigner used the wrong key when signing after first rollerd call
-
+if [ $SKIPZONESIGNER != 1 ]; then
+  $ZONESIGNER example.com
+  checkkeyid $curkeyid zonesigner used the wrong key when signing after first rollerd call
+fi
 
 # run again 60 seconds later
 log "sleeping for 60 to let rollerd go to the next phase -> 1"
@@ -111,9 +119,10 @@ checkrollerdphase 1
 checkkeyid $curkeyid rollerd updated the key one phase too quickly "(in phase 1)"
 
 # check that zonesigner does the right thing and uses the current key too
-$ZONESIGNER example.com
-checkkeyid $curkeyid zonesigner failed to use the curkey that rollerd used
-
+if [ $SKIPZONESIGNER != 1 ]; then
+  $ZONESIGNER example.com
+  checkkeyid $curkeyid zonesigner failed to use the curkey that rollerd used
+fi
 
 
 # run again in phase 2 (we hope)
@@ -124,10 +133,11 @@ checkrollerdphase 3
 checkkeyid $pubkeyid rollerd should have started using the pub key "(in phase 3)"
 
 # check that zonesigner does the right thing and uses the current key too
-$ZONESIGNER example.com
-checkkeyid $pubkeyid zonesigner should be using the pub key now
-log checked phase 3 '(right after 2)' for rollerd/zonesigner
-
+if [ $SKIPZONESIGNER != 1 ]; then
+  $ZONESIGNER example.com
+  checkkeyid $pubkeyid zonesigner should be using the pub key now
+  log checked phase 3 '(right after 2)' for rollerd/zonesigner
+fi
 
 
 # run again in phase 3 immediately (we hope)
@@ -137,20 +147,23 @@ checkrollerdphase 3
 checkkeyid $pubkeyid rollerd should have started using the pub key "(in phase 3)"
 
 # check that zonesigner does the right thing and uses the current key too
-$ZONESIGNER example.com
-checkkeyid $pubkeyid zonesigner should be using the pub key now
-log checked phase 3 for rollerd/zonesigner
-
+if [ $SKIPZONESIGNER != 1 ]; then
+  $ZONESIGNER example.com
+  checkkeyid $pubkeyid zonesigner should be using the pub key now
+  log checked phase 3 for rollerd/zonesigner
+fi
 
 # sleep and run again and pub should switch to current and we should
 # get a new pub
 sleep 60
-log "running rollerd again to jump from 2 to 3"
+log "running rollerd again to jump from 3 and resetting to 0"
 $ROLLERD
-checkrollerdphase 3
-checkkeyid $pubkeyid rollerd should have started using the pub key "(in phase 3)"
+checkrollerdphase 0
+checkkeyid $pubkeyid rollerd should now be using the old pub key as current "(in phase 0)"
 
 # check that zonesigner does the right thing and uses the current key too
-$ZONESIGNER example.com
-checkkeyid $pubkeyid zonesigner should be using the pub key now
-log checked phase 3 '(right after 2)' for rollerd/zonesigner
+if [ $SKIPZONESIGNER != 1 ]; then
+  $ZONESIGNER example.com
+  checkkeyid $pubkeyid zonesigner should be using the current key which was the pub key
+  log checked phase 0 for rollerd/zonesigner
+fi
