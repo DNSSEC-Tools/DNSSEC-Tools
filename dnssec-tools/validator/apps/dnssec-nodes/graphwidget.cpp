885ae60cbc5e4cce42e6765da12c682ece9457c9
hardaker <hardaker@c4029f9c-b713-0410-a448-fae9e31a314a>
2012-07-23 20:37:50 +0000

    remove some early testing code
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@6948 c4029f9c-b713-0410-a448-fae9e31a314a

--- dnssec-tools/validator/apps/dnssec-nodes/graphwidget.cpp
+++ dnssec-tools/validator/apps/dnssec-nodes/graphwidget.cpp
@@ -140,7 +140,6 @@ GraphWidget::GraphWidget(QWidget *parent, QLineEdit *editor, const QString &file
     connect(&m_pcapWatcher, SIGNAL(addNode(QString)), m_nodeList, SLOT(addNodesSlot(QString)));
     connect(&m_pcapWatcher, SIGNAL(addNodeData(QString, DNSData, QString)), m_nodeList, SLOT(addNodesData(QString,DNSData, QString)));
     connect(this, SIGNAL(openPcapDevice()), &m_pcapWatcher, SLOT(openDevice()));
-    m_pcapWatcher.setDeviceName("lo");
     m_pcapWatcher.start();
 #endif
 }
