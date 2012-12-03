2f1a02041a4fd470963c854a7081e4a793ab40e1
Wayne Morrison <tewok>
2012-12-03 17:58:41 +0000

    Example lines that may be added to Nagios' side.php file.

---
+++ dnssec-tools/apps/owl-monitor/manager/nagios-files/example-side.php
@@ -0,0 +1,33 @@
+
+<!---------------------------------------------------------------------------->
+
+<div class="navsection">
+<div class="navsectiontitle">UEM Graphs</div>
+<div class="navsectionlinks">
+
+<p>
+<owl style="font-size: 8pt;">DRRAW Graphing</owl>
+<ul class="navsectionlinks">
+<li><a href="/owl/cgi-bin/drraw.cgi" target="main" onMouseOver="switchdot('graphs-dot',1)" onMouseOut="switchdot('graphs-dot',0)" class="NavBar Item">Graphing Home</a>
+</ul>
+<p>
+<ul>
+<li><a href="/owl/cgi-bin/drraw.cgi?Mode=view;Graph=1351640492.19776" target="main" onMouseOver="switchdot('graphs-dot',1)" onMouseOut="switchdot('graphs-dot',0)" class="NavBar Item">DNS Response for Root from D Root (Kodiak Sensor)<br><i>example graph</i></a>
+</ul>
+</p>
+<p>
+
+
+<owl style="font-size: 8pt;">Nagiosgraph Graphing</owl>
+<ul>
+<li><a href="<?php echo $cfg["ng_cgi_base_url"];?>/show.cgi" target="<?php echo $link_target;?>">Graphs</a></li>
+<li><a href="<?php echo $cfg["ng_cgi_base_url"];?>/showhost.cgi" target="<?php echo $link_target;?>">Graphs by Host</a></li>
+<li><a href="<?php echo $cfg["ng_cgi_base_url"];?>/showservice.cgi" target="<?php echo $link_target;?>">Graphs by Service</a></li>
+</ul>
+</li>
+
+</ul>
+</div>
+
+<!---------------------------------------------------------------------------->
+
