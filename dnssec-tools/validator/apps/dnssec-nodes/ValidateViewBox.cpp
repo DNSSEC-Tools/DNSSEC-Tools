3ef2e6402cd6ed1ff3b43116726a0c2879fd9738
Wes Hardaker <hardaker>
2013-02-05 22:01:29 +0000

    Make the box selections also act on arrow colors when selected

--- dnssec-tools/validator/apps/dnssec-nodes/ValidateViewBox.cpp
+++ dnssec-tools/validator/apps/dnssec-nodes/ValidateViewBox.cpp
@@ -5,7 +5,7 @@
 #include <qdebug.h>
 
 ValidateViewBox::ValidateViewBox(qreal x, qreal y, qreal width, qreal height, QGraphicsItem *parent) :
-    QGraphicsRectItem(x,y,width,height,parent)
+    QGraphicsRectItem(x,y,width,height,parent), m_isSelected(false), m_lines(), m_paths()
 {
     setPen(QPen(Qt::black));
     QBrush thebrush = brush();
@@ -16,11 +16,22 @@ ValidateViewBox::ValidateViewBox(qreal x, qreal y, qreal width, qreal height, QG
 
 void ValidateViewBox::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
+    Q_UNUSED(event)
     setPen(QPen(Qt::blue));
     QBrush thebrush = brush();
     thebrush.setColor(QColor(Qt::blue).lighter());
     setBrush(thebrush);
+    m_isSelected = true;
+    foreach(QGraphicsLineItem *item, m_lines) {
+        item->setPen(QPen(Qt::blue));
+        item->update();
+    }
+    foreach(QGraphicsPathItem *item, m_paths) {
+        item->setPen(QPen(Qt::blue));
+        item->update();
+    }
     update();
+
     //QGraphicsItem::mousePressEvent(event);
 }
 
@@ -30,6 +41,15 @@ void ValidateViewBox::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
     QBrush thebrush = brush();
     thebrush.setColor(QColor(Qt::gray).lighter());
     setBrush(thebrush);
+    m_isSelected = false;
+    foreach(QGraphicsLineItem *item, m_lines) {
+        item->setPen(QPen(Qt::green));
+        item->update();
+    }
+    foreach(QGraphicsPathItem *item, m_paths) {
+        item->setPen(QPen(Qt::green));
+        item->update();
+    }
     update();
     QGraphicsItem::mouseReleaseEvent(event);
 }
--- dnssec-tools/validator/apps/dnssec-nodes/ValidateViewBox.h
+++ dnssec-tools/validator/apps/dnssec-nodes/ValidateViewBox.h
@@ -2,6 +2,7 @@
 #define VALIDATEVIEWBOX_H
 
 #include <QGraphicsRectItem>
+#include <QList>
 
 class ValidateViewBox : public QGraphicsRectItem
 {
@@ -9,6 +10,16 @@ public:
     ValidateViewBox ( qreal x, qreal y, qreal width, qreal height, QGraphicsItem * parent = 0 );
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
+
+    bool isSelected() const { return m_isSelected; }
+    void addLineObject(QGraphicsLineItem *item) { m_lines.append(item); }
+    void addPathObject(QGraphicsPathItem *item) { m_paths.append(item); }
+
+private:
+    bool m_isSelected;
+    QList<QGraphicsLineItem *> m_lines;
+    QList<QGraphicsPathItem *> m_paths;
+
 };
 
 #endif // VALIDATEVIEWBOX_H
