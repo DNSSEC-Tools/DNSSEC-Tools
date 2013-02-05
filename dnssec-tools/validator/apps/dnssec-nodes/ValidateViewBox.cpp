c407efb750bf5f2c97235a3176c25b759e45c4b0
Wes Hardaker <hardaker>
2013-02-05 22:01:00 +0000

    Make the validation box blue when click-held

--- dnssec-tools/validator/apps/dnssec-nodes/ValidateViewBox.cpp
+++ dnssec-tools/validator/apps/dnssec-nodes/ValidateViewBox.cpp
@@ -2,6 +2,7 @@
 
 #include <QPen>
 #include <QBrush>
+#include <qdebug.h>
 
 ValidateViewBox::ValidateViewBox(qreal x, qreal y, qreal width, qreal height, QGraphicsItem *parent) :
     QGraphicsRectItem(x,y,width,height,parent)
@@ -11,4 +12,24 @@ ValidateViewBox::ValidateViewBox(qreal x, qreal y, qreal width, qreal height, QG
     thebrush.setColor(QColor(Qt::gray).lighter());
     thebrush.setStyle(Qt::SolidPattern);
     setBrush(thebrush);
+}
+
+void ValidateViewBox::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    setPen(QPen(Qt::blue));
+    QBrush thebrush = brush();
+    thebrush.setColor(QColor(Qt::blue).lighter());
+    setBrush(thebrush);
+    update();
+    //QGraphicsItem::mousePressEvent(event);
+}
+
+void ValidateViewBox::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    setPen(QPen(Qt::black));
+    QBrush thebrush = brush();
+    thebrush.setColor(QColor(Qt::gray).lighter());
+    setBrush(thebrush);
+    update();
+    QGraphicsItem::mouseReleaseEvent(event);
 }
