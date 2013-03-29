e31d819cb7f48e30d1334e205f4b63242c4adb1f
hserus <hserus@c4029f9c-b713-0410-a448-fae9e31a314a>
2013-03-29 06:06:11 +0000

    Add examples for res_query and resolve_and_check
    git-svn-id: https://www.dnssec-tools.org/svn/dnssec-tools/trunk@7713 c4029f9c-b713-0410-a448-fae9e31a314a

---
+++ dnssec-tools/tools/modules/Net-DNS-SEC-Validator/examples.pl
@@ -0,0 +1,45 @@
+use Net::DNS::SEC::Validator;
+use Net::DNS::Packet;
+
+$validator = new Net::DNS::SEC::Validator(
+    nslist => "8.8.8.8",
+#    log_target => "7:stderr",
+    rec_fallback => 0,
+);
+
+$r = $validator->res_query("good-a.test.dnssec-tools.org", "IN", "A");
+my ($pkt, $err) = new Net::DNS::Packet(\$r);
+print ($validator->istrusted ? 
+           "result is trusted\n" : 
+           "result is NOT trusted\n");
+#$pkt->print;
+
+##################################################
+
+$a = $validator->resolve_and_check("badsign-a.test.dnssec-tools.org", "IN", "A", 0);
+#$a = $validator->resolve_and_check("good-a.test.dnssec-tools.org", "IN", "A", 0);
+foreach $h (@$a) {
+   print "Status: " .  ${$h}{status} . "\n"; 
+   print ($validator->istrusted(${$h}{status}) ? 
+           "result is trusted\n" : 
+           "result is NOT trusted\n");
+
+   $ac = ${$h}{answer}; 
+   do {
+        print "AC status: " . ${$ac}{status} . "\n";
+        $acr = ${$ac}{rrset};
+        $acd = ${$acr}{data};
+        foreach $d (@$acd) {
+            print "Data RR status: " . ${$d}{rrstatus} . "\n";
+#            ${$d}{rrdata}->print;
+        }
+        $acs = ${$acr}{sigs};
+        foreach $d (@$acs) {
+            print "Sig RR status: " . ${$d}{rrstatus} . "\n";
+#            ${$d}{rrdata}->print;
+        }
+        $ac = ${$ac}{trust};
+   } while ($ac);
+}
+
+
