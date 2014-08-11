c3edfaf2ea231110aac662d45d8a75a1018996ec
Suresh Krishnaswamy <hserus>
2014-08-11 22:23:20 +0000

    Bump up allocated block for memory for tsig key to be safe wrt strlen bounds.

--- dnssec-tools/validator/libsres/res_tsig.c
+++ dnssec-tools/validator/libsres/res_tsig.c
@@ -173,7 +173,7 @@ res_set_ns_tsig(struct name_server *ns, char *tsigstr)
     }
 
     /* Decode the base64 key */
-    tsig->key = (u_char *) MALLOC (strlen(key_s));
+    tsig->key = (u_char *) MALLOC (strlen(key_s)+1);
     if (tsig->key == NULL) {
         goto err;
     }
