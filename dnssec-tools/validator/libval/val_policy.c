5caa48c40f0f721b51f26c71eaffcda240936745
Wes Hardaker <hardaker>
2014-08-28 18:27:59 +0000

    define atexit macro with one arg (android only)

--- dnssec-tools/validator/libval/val_policy.c
+++ dnssec-tools/validator/libval/val_policy.c
@@ -28,7 +28,7 @@ const char resolv_conf_inline_buf[] = "";
 
 #ifdef ANDROID
 #include <sys/system_properties.h>
-#define atexit()
+#define atexit(func)
 #endif /* ANDROID */
 
 #define READ_POL_FOR_ZONE(buf_ptr, end_ptr, line_number, endst, retval, err, token)  do {\
